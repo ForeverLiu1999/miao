9f700df07ae77471939fa8c14a0cd51103ca7ea8
Ashenone <2657518095@qq.com>
2021-12-25 20:50:47 +0800

    3214

--- leetcodeEveryday/day53/66-E-加一.js
+++ leetcodeEveryday/day53/66-E-加一.js
@@ -2,21 +2,38 @@
  * @param {number[]} digits
  * @return {number[]}
  */
- var plusOne = function(digits) {
-  for (let i = digits.length - 1; i >= 0; i--) {
-    // 数字9时要进位，所以必为0
-    if (digits[i] == 9) {
+//  var plusOne = function(digits) {
+//   for (let i = digits.length - 1; i >= 0; i--) {
+//     // 数字9时要进位，所以必为0
+//     if (digits[i] == 9) {
+//       digits[i] = 0;
+//     } else {
+//       // 其他数字加一就好
+//       digits[i] ++;
+//       return digits;
+//     }
+//   }
+//   // // 当所有位都进位时，长度加一
+//   // digits = digits.length + 1;
+//   // // 而且说明首位为一
+//   // digits[0] = 1;
+//   digits.splice(0, 0, 1);
+//   return digits;
+// };
+
+var plusOne = function(digits) {
+  digits[digits.length - 1]++;
+  for (var i = digits.length - 1; i > 0; i--) {
+    if (digits[i] == 10) {
       digits[i] = 0;
+      digits[i - 1]++;
     } else {
-      // 其他数字加一就好
-      digits[i] ++;
       return digits;
     }
   }
-  // // 当所有位都进位时，长度加一
-  // digits = digits.length + 1;
-  // // 而且说明首位为一
-  // digits[0] = 1;
-  digits.splice(0, 0, 1);
+  if (digit[0] ==10) {
+    digits[0] = 0;
+    digits.unshift(1);
+  }
   return digits;
-};
+}
--- leetcodeEveryday/day54/202-E-快乐数.js
+++ leetcodeEveryday/day54/202-E-快乐数.js
@@ -2,17 +2,40 @@
  * @param {number} n
  * @return {boolean}
  */
- var isHappy = function(n) {
-  var sum = 0;
-  while (n > 0) {
-    var digit = n % 10;
-    sum += digit * digit;
-    n = (n - digit) / 10;
+//  var isHappy = function(n) {
+//   var sum = 0;
+//   while (n > 0) {
+//     var digit = n % 10;
+//     sum += digit * digit;
+//     n = (n - digit) / 10;
+//   }
+//   if (sum == 1) {
+//     return true;
+//   } else if (sum == 4) {
+//     return false;
+//   }
+//   return isHappy(sum);
+// };
+
+var isHappy = function(n) {
+  var appeared = [];
+  while(true) {
+    var sum = 0;
+    while(n > 0) {
+      var digit = n % 10;
+      sum += digit * digit;
+      n = (n - digit) / 10;
+    }
+    if (sum == 1) {
+      return true;
+    } else {
+      for (var i = 0; i < appeared.length; i++) {
+        if (sum == appeared[i]) {
+          return false;
+        }
+      }
+      appeared.push(sum);
+      n = sum;
+    }
   }
-  if (sum == 1) {
-    return true;
-  } else if (sum == 4) {
-    return false;
-  }
-  return isHappy(sum);
-};
+}
