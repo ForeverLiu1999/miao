996a3e94e3805fc974afa4cf60008683709ad092
Ashenone <2657518095@qq.com>
2021-12-25 15:41:18 +0800

    231

--- leetcodeEveryday/day54/167-E-两数之和II输入有序数组.js
+++ leetcodeEveryday/day54/167-E-两数之和II输入有序数组.js
@@ -21,17 +21,31 @@
 
 // 1.左右双指针，左指针选中第一个数右移，右指针选中最后一个数左移。如果左右指针和大于target，说明右指针需要左移。
 // 2.当两数之和小于target，说明此轮移动没找到左+右=target，所以左指针右移，注意两指针只往中间移动，不往两边移动。
-var twoSum = function(numbers, target) {
-  var i = 0;
-  var j = numbers.length - 1;
-  while(i < j) {
-    var sum = numbers[i] + numbers[j];
-    if (sum > target) {
-      j--;
-    } else if (sum < target) {
-      i++;
-    } else {
-      return [i + 1, j + 1];
+// var twoSum = function(numbers, target) {
+//   var i = 0;
+//   var j = numbers.length - 1;
+//   while(i < j) {
+//     var sum = numbers[i] + numbers[j];
+//     if (sum > target) {
+//       j--;
+//     } else if (sum < target) {
+//       i++;
+//     } else {
+//       return [i + 1, j + 1];
+//     }
+//   }
+// };
+
+var twoSum = function (numbers, target) {
+  var a = []; // 此数组记录某个数在原数组中的第几位
+  for (var i = 0; i < numbers.length; i++) {
+    var need = target - nums[i];
+    if (a[need] !== undefined) {
+      var pos = a[need]; // need这个数在原数组中的位置
+      return [i + 1, pos + 1];
+    } else { // 到目前为止还没有记录need在原数组的位置
+      a[nums[i]] = i; // 记录当前数在原数组中的位置
+
     }
   }
-};
+}
