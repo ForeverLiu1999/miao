a39622b4c5519e680700a8add71629ea87ba6ac4
Ashenone <2657518095@qq.com>
2021-12-25 14:31:49 +0800

    2131

--- leetcodeEveryday/day54/167-E-两数之和II输入有序数组.js
+++ leetcodeEveryday/day54/167-E-两数之和II输入有序数组.js
@@ -2,19 +2,36 @@
  * @param {number} n
  * @return {number[]}
  */
- var countBits = function(n) {
-  const bits = new Array(n + 1).fill(0);
-  for (let i = 0; i <= n; i++) {
-      bits[i] = countOnes(i);
-  }
-  return bits
-};
+//  var countBits = function(n) {
+//   const bits = new Array(n + 1).fill(0);
+//   for (let i = 0; i <= n; i++) {
+//       bits[i] = countOnes(i);
+//   }
+//   return bits
+// };
 
-const countOnes = (x) => {
-  let ones = 0;
-  while (x > 0) {
-      x &= (x - 1);
-      ones++;
+// const countOnes = (x) => {
+//   let ones = 0;
+//   while (x > 0) {
+//       x &= (x - 1);
+//       ones++;
+//   }
+//   return ones;
+// }
+
+// 1.左右双指针，左指针选中第一个数右移，右指针选中最后一个数左移。如果左右指针和大于target，说明右指针需要左移。
+// 2.当两数之和小于target，说明此轮移动没找到左+右=target，所以左指针右移，注意两指针只往中间移动，不往两边移动。
+var twoSum = function(numbers, target) {
+  var i = 0;
+  var j = numbers.length - 1;
+  while(i < j) {
+    var sum = numbers[i] + numbers[j];
+    if (sum > target) {
+      j--;
+    } else if (sum < target) {
+      i++;
+    } else {
+      return [i + 1, j + 1];
+    }
   }
-  return ones;
-}
+};
