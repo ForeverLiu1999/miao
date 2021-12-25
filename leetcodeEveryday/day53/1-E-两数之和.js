bb0ea35e2072f80a278cd66ac2a7dd399f372d0f
Ashenone <2657518095@qq.com>
2021-12-25 14:14:00 +0800

    123

--- leetcodeEveryday/day53/1-E-两数之和.js
+++ leetcodeEveryday/day53/1-E-两数之和.js
@@ -3,13 +3,23 @@
  * @param {number} target
  * @return {number[]}
  */
- var twoSum = function(nums, target) {
-  let map = new Map();
-  for (let i = 0; i < nums.length; i++) {
-    x = target - nums[i];
-    if (map.has(x)) {
-      return [map.get(x), i];
+//  var twoSum = function(nums, target) {
+//   let map = new Map();
+//   for (let i = 0; i < nums.length; i++) {
+//     x = target - nums[i];
+//     if (map.has(x)) {
+//       return [map.get(x), i];
+//     }
+//     map.set(nums[i], i);
+//   }
+// };
+
+var twoSum = function (nums, target) {
+  for(var i = 0; i < nums.length - 1; i++) {
+    for(var j = i + 1; j < nums.length; j++) {
+      if (nums[i] + nums[j] == target) {
+        return [i, j];
+      }
     }
-    map.set(nums[i], i);
   }
-};
+}
