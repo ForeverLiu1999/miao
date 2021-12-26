0dc431ecc3a5a1792d74942c896c2a0eaafb3146
Ashenone <2657518095@qq.com>
2021-12-26 17:29:22 +0800

    1234

--- leetcodeEveryday/day56/50-M-pow(x, n).js
+++ leetcodeEveryday/day56/50-M-pow(x, n).js
@@ -31,13 +31,31 @@
 
 // 递归1
 var myPow = function(x, n) {
-
+  if (n === 0) return 1;
+  // x的n次方等于1除以x的-n次方
+  if (n < 0) {
+    return 1 /myPow(x, -n);
+  }
+  // n为奇数时，x的n次方 = x*x的n-1次方
+  if (n % 2) {
+    return x * myPow(x, n - 1);
+  }
+  return myPow(x * x, n / 2); // n是偶数则分治,x*x的n/2次方
 };
+
+
 // 递归2
 var myPow = function(x, n) {
-
+  if (n < 0) {
+    return 1 / (x * myPow(x, -(n + 1)));
+  }
+  if (n === 0) {
+    return 1;
+  }
+  if (n === 1) {
+    return x;
+  }
+  return n % 2 === 1 ? x * myPow(x, n - 1) : myPow(x * x, n / 2);
 };
-
-// var myPow = function(x, n) {
-
-// };
+// 若奇数次幂，幂次-1，转成偶数次幂
+// 核心代码就是偶数次幂的调用 myPow(x * x, n / 2);
