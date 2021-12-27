f671586ee5687147f004cb31f5518a504c47fda1
Ashenone <2657518095@qq.com>
2021-12-27 22:26:18 +0800

    1243

--- leetcodeEveryday/day57/42-H-接雨水.js
+++ leetcodeEveryday/day57/42-H-接雨水.js
@@ -2,11 +2,10 @@
  * @param {number[]} height
  * @return {number}
  */
-
 // 双指针
 // 解题思路，以列为单位，每列雨水的高度取决于该列左侧最高的柱子和右侧最高的柱子中最矮的那个柱子的高度
 // min(lHeight, rHeight) - height
- var trap = function(height) {
+var trap = function(height) {
   let n = height.length;
   if (n == 0) {
     return 0;
@@ -16,7 +15,7 @@
   // 左侧最大值
   left_max[0] = height[0];
   for (let i = 1; i < n; i++) {
-    left_max[i] = Math(left_max[i - 1], height[i]);
+    left_max[i] = Math.max(left_max[i - 1], height[i]);
   }
   // 右侧最大值
   right_max[n - 1] = height[n - 1];
