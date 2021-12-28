58e10ab7c641484aab42c7eb640a4e65b2fcfd4c
Ashenone <2657518095@qq.com>
2021-12-28 21:45:02 +0800

    136

--- leetcodeEveryday/136-E-只出现一次的数字.js
+++ leetcodeEveryday/136-E-只出现一次的数字.js
@@ -2,10 +2,10 @@
  * @param {number[]} nums
  * @return {number}
  */
- var singleNumber = function(nums) {
-  let ans = 0;
-  for (num = 0; num < nums; num++) {
-    ans ^= num;
-  }
-  return ans;
-};
+//  var singleNumber = function(nums) {
+//   let ans = 0;
+//   for (num = 0; num < nums; num++) {
+//     ans ^= num;
+//   }
+//   return ans;
+// };
