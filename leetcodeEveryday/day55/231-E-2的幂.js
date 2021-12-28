c0320aa2af506bc6b66efd47f425b8b8ea98bf96
Ashenone <2657518095@qq.com>
2021-12-28 20:28:20 +0800

    124

--- leetcodeEveryday/day55/231-E-2的幂.js
+++ leetcodeEveryday/day55/231-E-2的幂.js
@@ -2,10 +2,18 @@
  * @param {number} n
  * @return {boolean}
  */
- var isPowerOfTwo = function(n) {
+//  var isPowerOfTwo = function(n) {
+//   if (n < 1) {
+//     return false;
+//   } while (n % 2 == 0) {
+//     n = n / 2;
+//   } return n == 1;
+// };
+
+// 因为2的n次幂1000000，2的n-1次幂0111111，所以如果两者按位&操作必为0
+var isPowerOfTwo = function (n) {
   if (n < 1) {
     return false;
-  } while (n % 2 == 0) {
-    n = n / 2;
-  } return n == 1;
-};
+  }
+  return (n & (n - 1)) == 0;
+}
