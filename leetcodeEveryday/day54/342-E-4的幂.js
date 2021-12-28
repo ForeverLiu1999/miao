6379bf7f4dbe2057062539ba9511990469f012ce
Ashenone <2657518095@qq.com>
2021-12-28 20:32:02 +0800

    342

--- leetcodeEveryday/day54/342-E-4的幂.js
+++ leetcodeEveryday/day54/342-E-4的幂.js
@@ -3,22 +3,21 @@
  * @return {boolean}
  */
 //  4的幂结尾都是4或6 4 16 64 256
- function isNotFour(num) {
-  const remain = num % 10;
-  if (remain === 4 || remain === 6) {
-    return true;
-  }
-  return false;
-}
-
-var isPowerOfFour = function(num) {
-  if (num === 1) return true;
-  if (num <= 0 || !isNotFour(num)) return false;
-  while (num > 1) {
-    if (!isNotFour(num)) return false;
-    let remain = num % 4;
-    if (remain !== 0) return false;
-    num = num / 4;
-  }
-  return true;
-};
+//  function isNotFour(num) {
+//   const remain = num % 10;
+//   if (remain === 4 || remain === 6) {
+//     return true;
+//   }
+//   return false;
+// }
+// var isPowerOfFour = function(num) {
+//   if (num === 1) return true;
+//   if (num <= 0 || !isNotFour(num)) return false;
+//   while (num > 1) {
+//     if (!isNotFour(num)) return false;
+//     let remain = num % 4;
+//     if (remain !== 0) return false;
+//     num = num / 4;
+//   }
+//   return true;
+// };
