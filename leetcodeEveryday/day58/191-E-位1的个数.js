a4c3082b286d58c96f9c98b7cf9baf250fb626a0
Ashenone <2657518095@qq.com>
2021-12-28 21:19:49 +0800

    123

--- leetcodeEveryday/day58/191-E-位1的个数.js
+++ leetcodeEveryday/day58/191-E-位1的个数.js
@@ -2,14 +2,23 @@
  * @param {number} n - a positive integer
  * @return {number}
  */
- var hammingWeight = function(n) {
+//  var hammingWeight = function(n) {
+//   var count = 0;
+//   while (n > 0) {
+//     var d = n % 2;
+//     if (d) {
+//       count++;
+//     }
+//     n = (n - d) / 2;
+//   }
+//   return count;
+// };
+
+var hammingWeight = function (n) {
   var count = 0;
   while (n > 0) {
-    var d = n % 2;
-    if (d) {
-      count++;
-    }
-    n = (n - d) / 2;
+    count++;
+    n = (n & (n - 1)) >>> 0;
   }
   return count;
-};
+}
