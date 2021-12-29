ea3e71123f188dfc14d1d8d29c510d5a08bf1c6d
Ashenone <2657518095@qq.com>
2021-12-29 20:18:19 +0800

    43

---
+++ leetcodeEveryday/day59/43-M-字符串相乘.js
@@ -0,0 +1,12 @@
+/**
+ * @param {string} num1
+ * @param {string} num2
+ * @return {string}
+ */
+ var multiply = function(num1, num2) {
+  let a = Number(num1);
+  let b = Number(num2);
+  let c = a * b;
+  let d = c + "";
+  return d;
+};
