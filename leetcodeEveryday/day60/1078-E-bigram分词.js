d92b0533b19b68390b2c410cf0da4dfeb103a573
Ashenone <2657518095@qq.com>
2021-12-30 22:49:17 +0800

    123

--- leetcodeEveryday/day60/1078-E-bigram分词.js
+++ leetcodeEveryday/day60/1078-E-bigram分词.js
@@ -4,6 +4,10 @@
  * @param {string} second
  * @return {string[]}
  */
+
+// 时间复杂度: O(N)
+// 空间复杂度: O(N)
+
  var findOcurrences = function(text, first, second) {
   // 拆分成数组
   const words = text.split(" ");
@@ -17,3 +21,19 @@
   }
   return ans;
 };
+
+var findOcurrences = function(text, first, second) {
+  const words = text.split(" ");
+  const list = [];
+  for (let i = 2; i < words.length; i++) {
+    if (words[i - 2] === first && words[i - 1] === second) {
+      list.push(words[i]);
+    }
+  }
+  const size = list.length;
+  const ret = Array(size).fill("");
+  for (let i = 0; i < size; i++) {
+    ret[i] = list[i];
+  }
+  return ret;
+}
