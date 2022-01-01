9523cb3d9ef6d42ca06805f658e5498ca5dbcecd
Ashenone <2657518095@qq.com>
2022-01-01 20:20:16 +0800

    53

--- leetcodeEveryday/day62/53-E-最大子数组和.js
+++ leetcodeEveryday/day62/53-E-最大子数组和.js
@@ -15,3 +15,12 @@ var maxSubArray = function (nums) {
   }
   return ans;
 };
+
+var maxSubArray = function (nums) {
+  let pre = 0; maxAns = nums[0];
+  nums.forEach((x) => {
+    pre = Math.max(pre + x, x);
+    maxAns = Math.max(maxAns, pre);
+  });
+  return maxAns;
+}
