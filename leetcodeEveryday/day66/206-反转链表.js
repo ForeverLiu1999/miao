29c4372b71ba5ce066dcfb9042e0efa50a61d4b2
Ashenone <2657518095@qq.com>
2022-01-05 10:49:52 +0800

    123

---
+++ leetcodeEveryday/day66/206-反转链表.js
@@ -0,0 +1,18 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+
+// https://leetcode-cn.com/problems/reverse-linked-list/solution/shi-pin-jiang-jie-die-dai-he-di-gui-hen-hswxy/
+
+
+ var reverseList = function(head) {
+
+};
--- leetcodeEveryday/day66/88-E-合并两个有序数组.js
+++ leetcodeEveryday/day66/88-E-合并两个有序数组.js
@@ -17,7 +17,32 @@
 // };
 
 var merge = function (nums1, m, nums2, n) {
-  let num = new Array(m + n - 3).fill(0);
-  num = nums1.concat(nums2);
-  return num;
+  nums1.splice(m, nums1.length - m, ...nums2);
+  nums1.sort((a, b) => a - b);
 };
+
+// 双指针法
+var merge = function (nums1, m , nums2, n) {
+// 双指针p1p2分别指向两个数组
+  let p1 = 0, p2 = 0;
+  // 先提前创建一个充满0的数组
+  const sorted = new Array(m + n).fill(0);
+  var cur;
+  // 双指针遍历范围
+  while (p1 < m || p2 < n) {
+    if (p1 === m) {
+      cur = nums2[p2++];;
+    } else if (p2 === n) {
+      cur = nums1[p1++];
+    } else if (nums1[p1] < nums2[p2]) {
+      cur = nums1[p1++];
+    } else {
+      cur = nums2[p2++];
+    }
+    sorted[p1 + p2 - 1] = cur;
+  }
+  // 把结果sorted填入nums1
+  for (let i = 0; i != m + n; ++i) {
+    nums1[i] = sorted[i];
+  }
+}
