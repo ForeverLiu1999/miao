3b72fcc39280126f87187e8fd009a3fb07306d18
Ashenone <2657518095@qq.com>
2022-01-05 10:57:25 +0800

    123

--- leetcodeEveryday/day66/206-反转链表.js
+++ leetcodeEveryday/day66/206-反转链表.js
@@ -12,7 +12,17 @@
 
 // https://leetcode-cn.com/problems/reverse-linked-list/solution/shi-pin-jiang-jie-die-dai-he-di-gui-hen-hswxy/
 
-
+// 迭代解法
+// curr.next = prev不断迭代向前移动可以直接反转,但如果直接修改curr.next，那就找不到下一个链表节点了也就无法迭代向前移动了，所以我们需要用next存储curr.next
+// 然后prev = curr，curr = next就能一直移动了
  var reverseList = function(head) {
-
+  let prev = null;
+  curr = head;
+  while (curr) {
+    const next = curr.next;
+    curr.next = prev;
+    prev = curr;
+    curr = next;
+  }
+  return prev;
 };
