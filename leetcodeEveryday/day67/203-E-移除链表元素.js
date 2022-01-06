81c77f317cd45f897d058c1c72694f60bfecdc73
Ashenone <2657518095@qq.com>
2022-01-06 20:12:38 +0800

    123

--- leetcodeEveryday/day67/203-E-移除链表元素.js
+++ leetcodeEveryday/day67/203-E-移除链表元素.js
@@ -13,7 +13,7 @@
 
 // 递归
 // 链表本身很适合递归，首先对除了头节点head之外的头节点进行删除操作，然后判断head的节点值是否等于给定的val
- var removeElements = function(head, val) {
+var removeElements = function (head, val) {
   //  递归终止条件是head为空
   if (head === null) {
     return head;
@@ -24,3 +24,46 @@
   // 则head保留，所以删除操作后的头节点还是head
   return head.val === val ? head.next : head;
 };
+
+// 迭代
+var removeElements = function (head, val) {
+  // 由于链表的头节点head有可能会被删除，所以创建哑结点有便于操作
+  const dummyHead = new ListNode(0);
+  // 并有如下
+  dummyHead.next = head;
+  // 初始化temp，temp表示当前节点
+  let temp = dummyHead;
+  // 如果temp的下一个节点不为空且下一个节点的节点值等于给定的val
+  while (temp.next !== null) {
+    if (temp.next.val == val) {
+      // 则需要删除下一个节点
+      temp.next = temp.next.next;
+      // 而如果temp的下一个节点的值不等于给定的val，则保留下一个节点，将temp移动到下一个节点
+    } else {
+      temp = temp.next;
+    }
+  }
+  // 最后返回新的头节点
+  return dummyHead.next;
+};
+
+// 由于链表的头节点head有可能会被删除，所以创建哑结点有便于操作
+// 并有如下
+// 初始化temp，temp表示当前节点
+// 如果temp的下一个节点不为空且下一个节点的节点值等于给定的val
+// 则需要删除下一个节点
+// 而如果temp的下一个节点的值不等于给定的val，则保留下一个节点，将temp移动到下一个节点
+// 最后返回新的头节点
+var removeElements = function (head, val) {
+  const dummyHead = new ListNode(0);
+  dummyHead.next = head;
+  let temp = dummyHead;
+  while(temp.next !== null) {
+    if (temp.next.val === val) {
+      temp.next = temp.next.next;
+    } else if (temp.next.val !== val) {
+      temp = temp.next;
+    }
+  }
+  return dummyHead.next;
+}
