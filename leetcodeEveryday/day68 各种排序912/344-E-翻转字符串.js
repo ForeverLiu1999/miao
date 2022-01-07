b98a66eff2b2a5a2f12ca9832e1bcc2c9141ae4a
Ashenone <2657518095@qq.com>
2022-01-07 22:20:17 +0800

    123

--- leetcodeEveryday/day68 各种排序912/344-E-翻转字符串.js
+++ leetcodeEveryday/day68 各种排序912/344-E-翻转字符串.js
@@ -3,5 +3,8 @@
  * @return {void} Do not return anything, modify s in-place instead.
  */
  var reverseString = function(s) {
-  let x = s.splice("")
+  const n = s.length;
+  for (let left = 0, right = n - 1; left < right; ++left, --right) {
+      [s[left], s[right]] = [s[right], s[left]];
+  }
 };
