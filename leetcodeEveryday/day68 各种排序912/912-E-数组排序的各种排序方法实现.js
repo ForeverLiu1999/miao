e029b315153d6d0603bc69d28d0dc58c6c617763
Ashenone <2657518095@qq.com>
2022-01-07 22:21:51 +0800

    123

--- leetcodeEveryday/day68 各种排序912/912-E-数组排序的各种排序方法实现.js
+++ leetcodeEveryday/day68 各种排序912/912-E-数组排序的各种排序方法实现.js
@@ -28,3 +28,19 @@ var sortArray = function (nums) {
   }
   return nums;
 }
+
+// 选择排序
+var sortArray = function(nums) {
+  for (let i = 0; i < nums.length; i++) {
+      let min = Infinity;
+      let minIndex;
+      for (j = i; j < nums.length; j++) {
+          if (nums[j] < min) {
+              min = nums[j]
+              minIndex = j;
+          }
+      }
+      [nums[i], nums[minIndex]] = [nums[minIndex], nums[i]];
+  }
+  return nums;
+};
