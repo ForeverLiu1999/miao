00780ee36d2794823f1400ce9c0e0e4db77e27d0
Ashenone <2657518095@qq.com>
2022-01-09 14:03:15 +0800

    876

--- leetcodeEveryday/day70/876-E-链表的中间节点.js
+++ leetcodeEveryday/day70/876-E-链表的中间节点.js
@@ -11,6 +11,8 @@
  */
 
 // 数组存储找中间元素
+// 时间复杂度O(N)
+// 空间复杂度O(N)
  var middleNode = function(head) {
   // 指针p指向链表头节点
   let p = [head];
@@ -21,3 +23,25 @@
   // 返回数组p的中间元素，如果偶数个就第二个
   return p[Math.trunc(p.length / 2)];
 };
+
+// 单指针法
+// 遍历两次链表第一次统计总共N个，第二次N/2那个则为中点
+// 时间复杂度O(N)
+// 空间复杂度O(1)
+var middleNode = function (head) {
+  n = 0;
+  cur = head;
+  // 第一次遍历，n用来计数
+  while (cur != null) {
+    ++n;
+    cur = cur.next;
+  }
+  k = 0;
+  cur = head;
+  // 第二次比那里就只遍历一半
+  while (k < Math.trunc(n / 2)) {
+    ++k;
+    cur = cur.next;
+  }
+  return cur;
+}
