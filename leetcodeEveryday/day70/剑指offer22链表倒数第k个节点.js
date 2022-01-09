642c3d45926f48bbfc912336d2c94c5831810f26
Ashenone <2657518095@qq.com>
2022-01-09 15:47:35 +0800

    offer22

--- leetcodeEveryday/day70/剑指offer22链表倒数第k个节点.js
+++ leetcodeEveryday/day70/剑指offer22链表倒数第k个节点.js
@@ -27,3 +27,19 @@
   }
   return slow;
 };
+
+// 第一次遍历长度，第二次取倒数k
+var getKthFromEnd = function (head, k) {
+  let node = head, n = 0;
+  // 第一次遍历
+  while (node) {
+    node = node.next;
+    n++;
+  }
+  node = head;
+  // 第二次直接取n-k
+  for (let i = 0; i < n - k; i++) {
+    node = node.next;
+  }
+  return node;
+}
