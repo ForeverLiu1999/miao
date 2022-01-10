b5bd5c6c77ba2c90eea6d87bdf6aa7736ebb8fbf
Ashenone <2657518095@qq.com>
2022-01-10 21:56:38 +0800

    82

--- leetcodeEveryday/day71/82-M-删除链表中重复元素II.js
+++ leetcodeEveryday/day71/82-M-删除链表中重复元素II.js
@@ -9,10 +9,14 @@
  * @param {ListNode} head
  * @return {ListNode}
  */
- var deleteDuplicates = function(head) {
-  // if (!head) {
-  //   return head;
-  // }
+// 重复元素必相邻，一次遍历解决
+// 时间复杂度O(n)
+// 空间复杂度O(1)
+var deleteDuplicates = function (head) {
+  // 防止head为空
+  if (!head) {
+    return head;
+  }
   // 创建哑结点
   const dummy = new ListNode(0, head);
   let cur = dummy;
@@ -20,7 +24,7 @@
   while (cur.next && cur.next.next) {
     // 如果有连续的值相等说明重复
     if (cur.next.val === cur.next.next.val) {
-      //
+      // 用x保存cur.next
       const x = cur.next.val;
       // 知道cur.next为空或者值不等于x为止
       while (cur.next && cur.next.val === x) {
@@ -35,3 +39,30 @@
   // 遍历完整个数组之后返回哑结点的下一个节点
   return dummy.next;
 };
+
+var deleteDuplicates = function (head) {
+  // 防止head为空
+  if (!head) {
+    return head;
+  }
+  const dummy = new ListNode(0, head);
+  let cur = dummy;
+  // cur.next以及cur.next可能为空产生报错
+  while (cur.next && cur.next.next) {
+  // 如果有连续的值相等说明重复
+    if (cur.next.val === cur.next.next.val) {
+      // 用x保存cur.next
+      const x = cur.next.val;
+  // 直到cur.next为空或者值不等于x为止
+      while (cur.next && cur.next.val === x) {
+  // 删除元素值为x的节点
+        cur.next = cur.next.next;
+      }
+    } else {
+      // 下一步
+      cur = cur.next;
+    }
+  }
+  // 遍历完整个数组之后返回哑结点的下一个节点
+  return dummy.next;
+}
