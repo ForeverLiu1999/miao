652d501cd973b052858267a8b52ade10296073e7
Ashenone <2657518095@qq.com>
2022-01-10 22:17:16 +0800

    86

---
+++ leetcodeEveryday/day71/86-M-分隔链表.js
@@ -0,0 +1,35 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} x
+ * @return {ListNode}
+ */
+ var partition = function(head, x) {
+  // 创建虚拟节点pApB，和两个链表ab
+  let pA = a = ListNode(0);
+  let pB = b = ListNode(0);
+  // 如果head存在
+  while (head) {
+    // 值小于x的加入a链表
+    if (head.val < x) {
+      a = a.next = head;
+      // 值大于x的加入b链表
+    } else if (head.val > x) {
+      b = b.next = head;
+    }
+    // 下一步
+    head = nead.next;
+    // a是在a链表尾部，所以连上b链表的头
+    a.next = pB.next;
+    // 而b是B链表的尾，所以连上null
+    b.next = null;
+    // 返回虚拟节点pA的next就是新链表
+    return pA.next;
+  }
+};
