50a845166742705807bcba796eb060a0f97ed661
Ashenone <2657518095@qq.com>
2022-01-14 14:55:31 +0800

    606

--- leetcodeEveryday/day75/606-E-根据二叉树创建字符串.js
+++ leetcodeEveryday/day75/606-E-根据二叉树创建字符串.js
@@ -10,6 +10,36 @@
  * @param {TreeNode} root
  * @return {string}
  */
- var tree2str = function(root) {
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} t
+ * @return {string}
+ */
 
+// 递归
+var tree2str = function (t) {
+  if (!t) return '';
+  // 转化为字符串
+  let str = t.val.toString();
+  // 左子树不为空
+  if (t.left) {
+    str = str + '(' + tree2str(t.left) + ')';
+  }
+  // 右子树不空
+  if (t.right) {
+    // 左子树为空右子树不空时，左子树的括号对不能省略，无左孩子，而只有右孩子，就加一层括号表示左孩子存在
+    if (!t.left) {
+      str += '()';
+    }
+    // 再对右孩子递归，并套一层外层括号
+    str = str + '(' + tree2str(t.right) + ')';
+  }
+  // 没有孩子就不加括号
+  return str;
 };
