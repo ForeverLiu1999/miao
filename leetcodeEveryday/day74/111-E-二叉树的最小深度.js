4952058cb233750624370c20ed4b20149218b09c
Ashenone <2657518095@qq.com>
2022-01-14 11:37:41 +0800

    111

--- leetcodeEveryday/day74/111-E-二叉树的最小深度.js
+++ leetcodeEveryday/day74/111-E-二叉树的最小深度.js
@@ -18,35 +18,15 @@
     return 0;
   }
   // 左右子树都为空，说明此处数到此为止，高度为1,而root不为null，所以1是最小值
-  if (root.left == null && root.right == null) {
+  if (!root.left && !root.right) {
     return 1;
   }
-  // 不能理解
-  let ans = Number.MAX_SAFE_INTEGER;
   // 计算左子树最小深度
-  if (root.left != null) {
-    ans = Math.min(minDepth(root.left), ans);
+  if (!root.left) {
+    return 1 + minDepth(root.right);
   }
   // 计算右子树最小深度
-  if (root.right != null) {
-    ans = Math.min(minDepth(root.right), ans);
+  if (!root.right) {
+    return 1 + minDepth(root.left);
   }
-  // +1表示当前节点存在一个深度
-  return ans + 1;
 };
-
-// 简化代码变好看
-var minDepth = function (root) {
-  // root为空时，说明高度0，高度都是大于等于0的数，所以0是最小值
-  if (!root) {
-    return 0;
-  // 计算左子树最小深度
-  } else if (root.left == null) {
-    return minDepth(root.right) + 1;
-  // 计算右子树最小深度
-  } else if (root.right == null) {
-    return minDepth(root.left) + 1;
-  }
-  // +1表示当前节点存在一个深度
-  return Math.min(minDepth(root.left), minDepth(root.right)) + 1;
-}
