d6b80f000b243571839a92eb38b937a2dd642396
Ashenone <2657518095@qq.com>
2022-01-14 22:18:36 +0800

    103

--- leetcodeEveryday/day75/103-M-二叉树的锯齿形层序遍历.js
+++ leetcodeEveryday/day75/103-M-二叉树的锯齿形层序遍历.js
@@ -10,34 +10,32 @@
  * @param {TreeNode} root
  * @return {number[][]}
  */
- const levelOrderBottom = (root) => {
-  if (!root) {
-    return [];
-  }
-  // 创建队列，层序遍历时，队列很好用
-  const queue = [];
-  // 把根push进来
-  queue.push(root);
-  // 创建一维数组
+
+var zigzagLevelOrder = function (root) {
   const res = [];
-  while (queue.length) {
-    // 创建二维数组
-    const subRes = [];
-    const levelSize = queue.length;
-    for (let i = 0; i < levelSize; i++) {
-      // 按照队列顺序弹出
-      const cur = queue.shift();
-      // 二维数组内push值
-      subRes.push(cur.val);
-      // 如果存在node的左右孩子就push进去
-      if (cur.left) {
-        queue.push(cur.left);
-      }
-      if (cur.right) {
-        queue.push(cur.right);
-      }
+  if (root == null) {
+    return res;
+  }
+  // 存储当前层的节点
+  let curLevel = [root];
+  while(curLevel.length) {
+    // 存放下一层的节点
+    const nextLevel = [];
+    // 存放当前层的节点值
+    const curLevelVals = [];
+    for (const node of curLevel) {
+      // 遍历当前层，将节点的值push进curLevelVals
+      curLevelVals.push(node.val);
+      // 然后把左右孩子节点push进下一层nextLevel中
+      node.left && nextLevel.push(node.left);
+      node.right && nextLevel.push(node.right);
     }
-    res.unshift(subRes);
+    // 当前层遍历结束，把整个层加入res
+    res.push(curLevelVals);
+    // 偶数层反转
+    res.length % 2 == 0 && curLevelVals.reverse();
+    // 更新
+    curLevel = nextLevel;
   }
   return res;
 }
