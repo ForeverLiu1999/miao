694715b843ef46afefc2477b3782415490509920
Ashenone <2657518095@qq.com>
2022-01-14 16:01:48 +0800

    103

--- leetcodeEveryday/day75/103-M-二叉树的锯齿形层序遍历.js
+++ leetcodeEveryday/day75/103-M-二叉树的锯齿形层序遍历.js
@@ -10,6 +10,34 @@
  * @param {TreeNode} root
  * @return {number[][]}
  */
- var zigzagLevelOrder = function(root) {
-
-};
+ const levelOrderBottom = (root) => {
+  if (!root) {
+    return [];
+  }
+  // 创建队列，层序遍历时，队列很好用
+  const queue = [];
+  // 把根push进来
+  queue.push(root);
+  // 创建一维数组
+  const res = [];
+  while (queue.length) {
+    // 创建二维数组
+    const subRes = [];
+    const levelSize = queue.length;
+    for (let i = 0; i < levelSize; i++) {
+      // 按照队列顺序弹出
+      const cur = queue.shift();
+      // 二维数组内push值
+      subRes.push(cur.val);
+      // 如果存在node的左右孩子就push进去
+      if (cur.left) {
+        queue.push(cur.left);
+      }
+      if (cur.right) {
+        queue.push(cur.right);
+      }
+    }
+    res.unshift(subRes);
+  }
+  return res;
+}
