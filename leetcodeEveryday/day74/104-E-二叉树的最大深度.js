3aa153bfc186c56ea2190853065d2d2bddcef571
Ashenone <2657518095@qq.com>
2022-01-14 11:32:45 +0800

    111

--- leetcodeEveryday/day74/104-E-二叉树的最大深度.js
+++ leetcodeEveryday/day74/104-E-二叉树的最大深度.js
@@ -12,15 +12,11 @@
  */
 
 // 递归
- var maxDepth = function(root) {
+var maxDepth = function (root) {
   //  递归最重要的终止条件，第一次生效就是在最大深度的叶子节点
   if (!root) {
     return 0;
-  } else {
-    // 分别求左右子树最大高度
-    const left = maxDepth(root.left);
-    const right = maxDepth(root.right);
-    // 本身也算高度，所以+1
-    return Math.max(left, right) + 1;
   }
+  // 本身也算高度，所以+1
+  return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
 };
