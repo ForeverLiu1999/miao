a50ea2be1edf2c2b04a98856183f55e86fe831fa
Ashenone <2657518095@qq.com>
2022-01-15 21:12:33 +0800

    747

---
+++ leetcodeEveryday/day76/747-E-至少是其他数字两倍的最大数.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+
+var dominantIndex = function(nums) {
+  const n = nums.length;
+  if(n == 1) return 0;
+  // a比b慢，所以b指向最大值，a指向次大值
+  let a = -1, b = 0;
+  // 进行遍历
+  for (let i = 1; i < n; i++){
+      if (nums[i] > nums[b]) {
+          a = b; b = i;
+      } else if (a == -1 || nums[i] > nums[a]) {
+          a = i;
+      }
+  }
+  // 最大值是否为次大值的两倍
+  return nums[b] >= nums[a] * 2 ? b : -1;
+};
