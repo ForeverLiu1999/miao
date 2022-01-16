755b48fea8ce5504c2f80180f64239732b135d96
Ashenone <2657518095@qq.com>
2022-01-16 16:06:42 +0800

    树转数组

--- leetcodeEveryday/day77 二叉树相关.js/二叉链表相关.js
+++ leetcodeEveryday/day77 二叉树相关.js/二叉链表相关.js
@@ -12,3 +12,16 @@ function aryToTree(ary, rootPos = 0) {
   rootNode.right = rightTree;
   return rootNode;
 }
+
+// 树转换为数组
+function treeToAry(tree, index = 0, ary = []) {
+  // 简写，隐式地包含了if (!tree) {
+  // return ary;
+  // }
+  if (tree) {
+    ary[index] = tree.val; // 将根节点放入对应位置
+    treeToAry(tree.left, index * 2 + 1, ary); // 将左子树放入
+    treeToAry(tree.right, index * 2 + 2, ary); // 将右子树放入
+  }
+  return ary;
+}
