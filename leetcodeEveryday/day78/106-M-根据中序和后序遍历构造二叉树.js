3a949420c744156c48e10849d0d8dc193d57e5dc
Ashenone <2657518095@qq.com>
2022-01-17 21:41:28 +0800

    105 106 over

--- leetcodeEveryday/day78/106-M-根据中序和后序遍历构造二叉树.js
+++ leetcodeEveryday/day78/106-M-根据中序和后序遍历构造二叉树.js
@@ -12,14 +12,16 @@
  * @return {TreeNode}
  */
 //  中序遍历左根右
- var buildTree = function(inorder, postorder) {
+var buildTree = function (inorder, postorder) {
   if (!postorder.length) return null;
-//  后序遍历左右根 所以postorder[postorder.length - 1]就是root
+  //  后序遍历左右根 所以postorder[postorder.length - 1]就是root
   let root = new TreeNode(postorder[postorder.length - 1]);
   // 在中序遍历序列中找值为root.val的索引
   let index = inorder.findIndex(number => number === root.val);
+  // 中序遍历左根右 后序遍历左右根
+  // inorder.slice(0, index)是中序遍历的左子树，postorder.slice(0, index)是后序遍历的左子树
+  // inorder.slice(index + 1, inorder.length)是中序遍历的右子树，postorder.slice(index, postorder.length - 1)是后序遍历的右子树
   root.left = buildTree(inorder.slice(0, index), postorder.slice(0, index));
   root.right = buildTree(inorder.slice(index + 1, inorder.length), postorder.slice(index, postorder.length - 1));
-
-  return root
+  return root;
 };
