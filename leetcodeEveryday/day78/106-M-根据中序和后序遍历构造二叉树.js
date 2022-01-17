28541da4ac9593f5c51156a119cb4bbe771d0181
Ashenone <2657518095@qq.com>
2022-01-17 21:37:35 +0800

    105 106

---
+++ leetcodeEveryday/day78/106-M-根据中序和后序遍历构造二叉树.js
@@ -0,0 +1,25 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {number[]} inorder
+ * @param {number[]} postorder
+ * @return {TreeNode}
+ */
+//  中序遍历左根右
+ var buildTree = function(inorder, postorder) {
+  if (!postorder.length) return null;
+//  后序遍历左右根 所以postorder[postorder.length - 1]就是root
+  let root = new TreeNode(postorder[postorder.length - 1]);
+  // 在中序遍历序列中找值为root.val的索引
+  let index = inorder.findIndex(number => number === root.val);
+  root.left = buildTree(inorder.slice(0, index), postorder.slice(0, index));
+  root.right = buildTree(inorder.slice(index + 1, inorder.length), postorder.slice(index, postorder.length - 1));
+
+  return root
+};
