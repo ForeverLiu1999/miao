9aa3e690d2c9a16479b91cc47fbe4b7264201889
Ashenone <2657518095@qq.com>
2022-01-17 15:53:13 +0800

    同上

--- leetcodeEveryday/day78/71-M-简化路径.js
+++ leetcodeEveryday/day78/71-M-简化路径.js
@@ -2,19 +2,23 @@
  * @param {string} path
  * @return {string}
  */
- var simplifyPath = function(path) {
+var simplifyPath = function (path) {
   // 将路径切成若干字符串组成的列表
   // 其中只有 空字符串,.,..,目录名(其中为数组字母下划线)
   const nums = path.split("/");
   // 用栈维护目录名
   const stack = [];
   for (const num of nums) {
-  // 而..则让目录返回上一级，只要栈不为空就pop栈顶，而遇到目录名就入栈
+    // 空字符串和.不用管
+    if (num === "" || num === ".") {
+      continue;
+    }
+    // 而..则让目录返回上一级，只要栈不为空就pop栈顶
     if (num === "..") {
       if (stack.length) {
         stack.pop();
       }
-  // 空字符串和.不用管
+      // 遇到目录名就入栈
     } else if (num.length && num !== ".") {
       stack.push(num);
     }
