8beb1a3dedf5e511e7b52a1c10a70768cf1e444d
Ashenone <2657518095@qq.com>
2022-01-19 22:32:02 +0800

    108

--- leetcodeEveryday/day80/108-E-将有序数组转换为二叉搜索树.js
+++ leetcodeEveryday/day80/108-E-将有序数组转换为二叉搜索树.js
@@ -11,6 +11,18 @@
  * @param {number[]} nums
  * @return {TreeNode}
  */
+// 因为数组是升序的，所以取中点后左右两边差不会大于1，满足高度差的条件，前一半左子树，后一半右子树
+// 时间复杂度 O(N)
+// 空间复杂度 每次递归都copy了N空间 O(N^2)
  var sortedArrayToBST = function(nums) {
-
+   if (nums.length === 0) return null;
+  //  取中点
+  const mid = nums.length >> 1;
+  // 创建一个新树，把nums[mid]放进去
+  const root = new TreeNode(nums[mid]);
+  // 左子树递归
+  root.left = sortedArrayToBST(nums.slice(0, mid));
+  // 右子树递归
+  root.right = sortedArrayToBST(nums.slice(mid + 1));
+  return root;
 };
