2d6c4ee9697da0d8373f03d9cf28f85f08e61dd8
Ashenone <2657518095@qq.com>
2022-01-19 14:38:21 +0800

    108 109 450 create

---
+++ leetcodeEveryday/day80/109-M-有序链表转换二叉搜索树.js
@@ -0,0 +1,23 @@
+// 109-M-有序链表转换二叉搜索树
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {TreeNode}
+ */
+ var sortedListToBST = function(head) {
+
+};
