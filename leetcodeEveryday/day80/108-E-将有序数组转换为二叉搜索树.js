e901bcf5d6d1391ae88193e1b4e84a9cd84d0404
Ashenone <2657518095@qq.com>
2022-01-20 09:36:26 +0800

    108-E-将有序数组转换为二叉搜索树

--- leetcodeEveryday/day80/108-E-将有序数组转换为二叉搜索树.js
+++ leetcodeEveryday/day80/108-E-将有序数组转换为二叉搜索树.js
@@ -26,3 +26,12 @@
   root.right = sortedArrayToBST(nums.slice(mid + 1));
   return root;
 };
+
+var sortedArrayToBST = function (nums) {
+  if (nums.length === 0) return null;
+  const mid = nums.length >> 1;
+  const root = new TreeNode(nums[mid]);
+  root.left = sortedArrayToBST(nums.slice(0, mid));
+  root.right = sortedArrayToBST(nums.slice(mid + 1));
+  return root;
+}
--- leetcodeEveryday/day80/450-M-删除二叉搜索树中的节点.js
+++ leetcodeEveryday/day80/450-M-删除二叉搜索树中的节点.js
@@ -12,6 +12,46 @@
  * @param {number} key
  * @return {TreeNode}
  */
- var deleteNode = function(root, key) {
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} key
+ * @return {TreeNode}
+ */
+ var deleteNode = function (root, key) {
+  //  情况一 说明没找到要删除的节点，返回空
+  if (!root) return root;
+  // 隐含情况二 左右孩子都为空 直接删除节点 返回null为根节点
 
+  if (root.val === key) {
+    // 删除节点的左孩子为空但右孩子不为空，删除节点，右孩子补位，返回右孩子为根节点
+      if (!root.left) return root.right;
+    // 删除节点的右孩子为空但左孩子不为空，删除节点，左孩子补位，返回左孩子为根节点
+      else if (!root.right) return root.left;
+      // 情况五 左右孩子都不为空，则将删除节点的左子树头节点(左孩子)放到删除节点的右子树的最左面节点的左孩子上
+      // 返回删除节点右孩子为新的根节点
+      else {
+          let cur = root.right;
+          while (cur.left) {
+              cur = cur.left;
+          }
+          cur.left = root.left;
+          root = root.right;
+          delete root;
+          return root;
+      }
+  }
+  // 通过递归返回值删除节点
+  if (root.val > key)
+      root.left = deleteNode(root.left, key);
+  if (root.val < key)
+      root.right = deleteNode(root.right, key);
+  return root;
 };
