b16d6f3330bb272c19683891375d5ec49ce480fb
Ashenone <2657518095@qq.com>
2022-01-24 22:16:08 +0800

    655-M-输出二叉树

---
+++ leetcodeEveryday/day85/655-M-输出二叉树.js
@@ -0,0 +1,16 @@
+// 655-M-输出二叉树
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {string[][]}
+ */
+ var printTree = function(root) {
+
+};
