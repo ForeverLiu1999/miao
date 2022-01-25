14f0c357fd28a4d9d63f3b5e571544edb84d02e8
Ashenone <2657518095@qq.com>
2022-01-25 21:47:42 +0800

    // offer03-E-数组中重复的数字

--- leetcodeEveryday/day86/offer03-E-数组中重复的数字.js
+++ leetcodeEveryday/day86/offer03-E-数组中重复的数字.js
@@ -11,3 +11,24 @@
   }
   return null;
 };
+
+var findRepeatNumber = function (nums) {
+  let map = {};
+  for (let num of nums) {
+    if (!map[num]) {
+      map[num] = true;
+    } else {
+      return num;
+    }
+  }
+}
+
+// set中无重复，用长度判断
+var findRepeatNumber = function (nums) {
+  let set = new Set();
+  for (let i in nums) {
+    var l = set.size;
+    set.add(nums[i]);
+    if (set.size === l) return nums[i];
+  }
+}
