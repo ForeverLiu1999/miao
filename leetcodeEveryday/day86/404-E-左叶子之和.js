336344f0e5bb0896bcabbf07f92df48f3a621a74
Ashenone <2657518095@qq.com>
2022-01-25 11:32:51 +0800

    // 404-E-左叶子之和

--- leetcodeEveryday/day86/404-E-左叶子之和.js
+++ leetcodeEveryday/day86/404-E-左叶子之和.js
@@ -33,3 +33,32 @@
 // function isSimpleNode (node) {
 //   return node && node.left === null && node.right === null;
 // }
+
+// BFS
+// 创建一个队列去遍历节点
+const sumOfLeftLeaves = (root) => {
+  if (root == null) return 0;
+  // 左叶子和
+  let sum = 0;
+  // 创建队列
+  const queue = [];
+  // 把root push进去
+  queue.push(root);
+  // 队列如果存在就把节点弹出
+  while (queue.length) {
+    const cur = queue.shift();
+    // 左右孩子入列
+    if (cur.left) {
+      // 如果左孩子节点是左叶子节点就把值加到sum上
+      if (cur.left.left == null && cur.left.right == null) {
+        sum += cur.left.val;
+      } else {
+        queue.push(cur.left);
+      }
+    }
+    if (cur.right) {
+      queue.push(cur.right);
+    }
+  }
+  return sum;
+}
