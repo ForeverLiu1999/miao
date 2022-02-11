dfe49bbc2ccfb19bff955a93fa7ace92adc252de
Ashenone <2657518095@qq.com>
2022-02-11 22:26:30 +0800

    // 定义两个栈，一个入队栈，一个出队栈

---
+++ leetcodeEveryday/day102/offer09-用两个栈实现队列.js
@@ -0,0 +1,40 @@
+// 定义两个栈，一个入队栈，一个出队栈
+var CQueue = function () {
+  this.stackA = [];
+  this.stackB = [];
+};
+/**
+ * @param {number} value
+ * @return {void}
+ */
+
+CQueue.prototype.appendTail = function (value) {
+  this.stackA.push(value);
+};
+
+/**
+ * @return {number}
+ */
+
+CQueue.prototype.deleteHead = function () {
+  // 如果有元素存在
+  if (this.stackB.length) {
+    // pop出该元素
+    return this.stackB.pop();
+  } else {
+    while (this.stackA.length) {
+      this.stackB.push(this.stackA.pop());
+    }
+    if (!this.stackB.length) {
+      return -1;
+    } else {
+      return this.stackB.pop();
+    }
+  }
+};
+/**
+ * Your CQueue object will be instantiated and called as such:
+ * var obj = new CQueue()
+ * obj.appendTail(value)
+ * var param_2 = obj.deleteHead()
+ */
