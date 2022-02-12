e36a13c8c35392fbcdb9ebba7d6a719016b2b9b6
Ashenone <2657518095@qq.com>
2022-02-12 21:52:47 +0800

    // 面试题10.01-E-合并排序的数组

--- leetcodeEveryday/day103/面试题10.01-E-合并排序的数组.js
+++ leetcodeEveryday/day103/面试题10.01-E-合并排序的数组.js
@@ -23,7 +23,7 @@ var merge = function (A, m, B, n) {
   // 限制指针的遍历范围
   while (pa < m || pb < n) {
     // 如果A中遍历完了，就去取B中值
-    if (pa === m ) {
+    if (pa === m) {
       cur = B[pb++];
     // 而如果B中遍历完了，就去取A中值
     } else if (pb === n) {
@@ -39,6 +39,27 @@ var merge = function (A, m, B, n) {
   }
   // 把sorted数组中所有元素放入A中
   for (let i = 0; i != m + n; ++i) {
+    A[i] = sorted[i];
+  }
+}
+
+var merge = function (A, m, B, n) {
+  let pa = 0, pb = 0;
+  const sorted = new Array(m + n).fill(0);
+  var cur;
+  while (pa < m || pb < n) {
+    if (pa === m) {
+      cur = B[pb++];
+    } else if (pb === n) {
+      cur = A[pa++];
+    } else if (A[pa] < B[pb]) {
+      cur = A[pa++];
+    } else {
+      cur = B[pb++];
+    }
+    sorted[pa + pb - 1] = cur;
+  }
+  for (let i =0; i != m + n; ++i) {
     A[i] = sorted[i];
   }
 }
