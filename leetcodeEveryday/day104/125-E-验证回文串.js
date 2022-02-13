bdf3bcc89020ea30bf3f1d55eda7a3cd7fa28dbb
Ashenone <2657518095@qq.com>
2022-02-13 16:58:53 +0800

    // 125-E-验证回文串

--- leetcodeEveryday/day104/125-E-验证回文串.js
+++ leetcodeEveryday/day104/125-E-验证回文串.js
@@ -23,3 +23,19 @@
   }
   return true
   };
+
+  var isPalindrome = function (s) {
+    var a = s.toLocaleLowerCase().match(/[A-Za-z0-9]+/g);
+    if(!a) return true;
+    var slong = a.join("");
+    var left = 0; right = slong.length - 1;
+    while(left < right) {
+      if (slong[left] == slong[right]) {
+        left++;
+        right--;
+      } else {
+        return false;
+      }
+    }
+    return true;
+  }
---
+++ leetcodeEveryday/day104/offer-10-斐波那契数列.js
@@ -0,0 +1,8 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+//  答案需要取模 1e9+7（1000000007），如计算初始结果为：1000000008，请返回 1。
+ var fib = function(n) {
+
+};
