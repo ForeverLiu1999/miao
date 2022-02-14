01d89d71cddc568b9ff0b83198386310de10cc24
Ashenone <2657518095@qq.com>
2022-02-14 22:38:43 +0800

    offer 21-E-调整数组顺序使奇数位于偶数前面

---
+++ leetcodeEveryday/day105/offer 21-E-调整数组顺序使奇数位于偶数前面.js
@@ -0,0 +1,15 @@
+// offer 21-E-调整数组顺序使奇数位于偶数前面
+  /**
+   * @param {number[]} nums
+   * @return {number[]}
+   */
+  var exchange = function(nums) {
+    let odd = [];
+    let even = [];
+    let result = [];
+    for (let i = 0; i <= nums.length; i++) {
+      if (nums[i] % 2 == 0) even.push(nums[i]);
+      if (nums[i] % 2 == 1) odd.push(nums[i]);
+    }
+    return result = odd.concat(even);
+  };
