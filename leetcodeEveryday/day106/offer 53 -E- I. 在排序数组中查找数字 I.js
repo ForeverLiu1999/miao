a543a283e8921064ae476fa751d6d7d6f6971a37
Ashenone <2657518095@qq.com>
2022-02-15 22:03:24 +0800

    offer 53 -E- I. 在排序数组中查找数字 I

--- leetcodeEveryday/day106/offer 53 -E- I. 在排序数组中查找数字 I.js
+++ leetcodeEveryday/day106/offer 53 -E- I. 在排序数组中查找数字 I.js
@@ -1,3 +1,4 @@
+// offer 53 -E- I. 在排序数组中查找数字 I
 /**
  * @param {number[]} nums
  * @param {number} target
@@ -10,3 +11,17 @@
   }
   return counter;
 };
+
+
+var search = function (nums, target) {
+  // 定义ans为数字出现次数
+  let ans = 0;
+  // 定义第一个大于等于target的下标和第一个大于target的下标
+  const leftIdx = binarySearch(nums, target, true);
+  const rightIdx = binarySearch(nums, target, false) - 1;
+  // 符合条件时返回差，不符合时返回0
+  if (leftIdx <= rightIdx && rightIdx < nums.length && nums[leftIdx] === target && nums[rightIdx] === target) {
+    ans = rightIdx - leftIdx + 1;
+  }
+  return ans;
+}
