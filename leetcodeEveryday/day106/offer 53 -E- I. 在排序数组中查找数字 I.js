9cb11af690b18394bf96b44a336f0142d59143c6
Ashenone <2657518095@qq.com>
2022-02-15 22:07:54 +0800

    offer 53 -E- I. 在排序数组中查找数字 I

--- leetcodeEveryday/day106/offer 53 -E- I. 在排序数组中查找数字 I.js
+++ leetcodeEveryday/day106/offer 53 -E- I. 在排序数组中查找数字 I.js
@@ -12,7 +12,20 @@
   return counter;
 };
 
-
+// 二分查找
+const binarySearch = (nums, target, lower) => {
+  let left = 0, right = nums.length - 1, ans = nums.length;
+  while (left <= right) {
+    const mid = Math.floor((left + right) / 2);
+    if (nums[mid] > target || (lower && nums[mid] >= target)) {
+      right = mid - 1;
+      ans = mid;
+    } else {
+      left = mid + 1;
+    }
+  }
+  return ans;
+}
 var search = function (nums, target) {
   // 定义ans为数字出现次数
   let ans = 0;
