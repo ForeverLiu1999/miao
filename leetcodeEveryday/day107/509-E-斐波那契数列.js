a55af3676b7bb7420c4cf077575afdbf11f0fefa
Ashenone <2657518095@qq.com>
2022-02-16 14:45:30 +0800

    // 509-E-斐波那契数列

--- leetcodeEveryday/day107/509-E-斐波那契数列.js
+++ leetcodeEveryday/day107/509-E-斐波那契数列.js
@@ -15,3 +15,16 @@
  }
  return b;
 };
+
+var fib = function(n) {
+  if (n < 2) {
+      return n;
+  }
+  let p = 0, q = 0, r = 1;
+  for (let i = 2; i <= n; i++) {
+      p = q;
+      q = r;
+      r = p + q;
+  }
+  return r;
+};
