e64279613da2e66ad5514ba8645212c1b4574ff5
Ashenone <2657518095@qq.com>
2022-02-16 15:09:34 +0800

    155-E-最小栈

---
+++ leetcodeEveryday/day107/155-E-最小栈.js
@@ -0,0 +1,46 @@
+// 155-E-最小栈
+// 设计一个数据结构，使得每个元素 a 与其相应的最小值 m 时刻保持一一对应。因此我们可以使用一个辅助栈，
+// 与元素栈同步插入与删除，用于存储与每个元素对应的最小值。
+// 当一个元素要入栈时，我们取当前辅助栈的栈顶存储的最小值，与当前元素比较得出最小值，将这个最小值插入辅助栈中；
+// 当一个元素要出栈时，我们把辅助栈的栈顶元素也一并弹出；
+// 在任意一个时刻，栈内元素的最小值就存储在辅助栈的栈顶元素中。
+var MinStack = function() {
+this.x_stack = [];
+this.min_stack = [Infiity];
+};
+
+MinStack.prototype.push = function(val) {
+this.x_stack = push(x);
+this.min_stack.push(Math.min(this.min_stack[this.min_stack.length - 1], x));
+};
+
+/**
+ * @return {void}
+ */
+MinStack.prototype.pop = function() {
+  this.x_stack.pop();
+  this.min_stack.pop();
+};
+
+/**
+ * @return {number}
+ */
+MinStack.prototype.top = function() {
+  return this.x_stack[this.x_stack.length - 1];
+};
+
+/**
+ * @return {number}
+ */
+MinStack.prototype.getMin = function() {
+return this.min_stack[this.min_stack.length - 1];
+};
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * var obj = new MinStack()
+ * obj.push(val)
+ * obj.pop()
+ * var param_3 = obj.top()
+ * var param_4 = obj.getMin()
+ */
