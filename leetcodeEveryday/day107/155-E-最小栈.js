ad843c00ef6c0e0fa18163984e69c530da0766b0
Ashenone <2657518095@qq.com>
2022-02-16 15:17:24 +0800

    // 155-E-最小栈

--- leetcodeEveryday/day107/155-E-最小栈.js
+++ leetcodeEveryday/day107/155-E-最小栈.js
@@ -5,42 +5,24 @@
 // 当一个元素要出栈时，我们把辅助栈的栈顶元素也一并弹出；
 // 在任意一个时刻，栈内元素的最小值就存储在辅助栈的栈顶元素中。
 var MinStack = function() {
-this.x_stack = [];
-this.min_stack = [Infiity];
+    this.x_stack = [];
+    this.min_stack = [Infinity];
 };
 
-MinStack.prototype.push = function(val) {
-this.x_stack = push(x);
-this.min_stack.push(Math.min(this.min_stack[this.min_stack.length - 1], x));
+MinStack.prototype.push = function(x) {
+    this.x_stack.push(x);
+    this.min_stack.push(Math.min(this.min_stack[this.min_stack.length - 1], x));
 };
 
-/**
- * @return {void}
- */
 MinStack.prototype.pop = function() {
-  this.x_stack.pop();
-  this.min_stack.pop();
+    this.x_stack.pop();
+    this.min_stack.pop();
 };
 
-/**
- * @return {number}
- */
 MinStack.prototype.top = function() {
-  return this.x_stack[this.x_stack.length - 1];
+    return this.x_stack[this.x_stack.length - 1];
 };
 
-/**
- * @return {number}
- */
 MinStack.prototype.getMin = function() {
-return this.min_stack[this.min_stack.length - 1];
+    return this.min_stack[this.min_stack.length - 1];
 };
-
-/**
- * Your MinStack object will be instantiated and called as such:
- * var obj = new MinStack()
- * obj.push(val)
- * obj.pop()
- * var param_3 = obj.top()
- * var param_4 = obj.getMin()
- */
