bd86d9911f120f87365cda7d0f7772a10ab44b36
Ashenone <2657518095@qq.com>
2022-02-18 15:59:15 +0800

    test

--- leetcodeEveryday/day108/剑指offer 53 - II. 0～n-1中缺失的数字.js
+++ leetcodeEveryday/day108/剑指offer 53 - II. 0～n-1中缺失的数字.js
@@ -1,3 +1,4 @@
+// 剑指offer 53 - II. 0～n-1中缺失的数字
 /**
  * @param {number[]} nums
  * @return {number}
@@ -9,3 +10,19 @@
  }
 };
 
+// 二分法
+// 确认一个元素，如果左侧无缺失则有nums[i] === i
+// 而如果不满足，缺失的数字在右边
+var missingNumber = function (nums) {
+  let left = 0;
+  let right = nums.length - 1;
+  while (left <= right) {
+    let mid = left + ((right - left) >>> 1);
+    if (mid == nums[mid]) {
+       left = mid + 1;
+    } else {
+      right = mid - 1;
+    }
+  }
+  return left;
+}
