7e50683451a5129ceaf1a7df046ed7aee6f35099
Ashenone <2657518095@qq.com>
2022-02-19 22:26:59 +0800

    // 11-M-盛最多水的容器

--- leetcodeEveryday/day110/11-M-盛最多水的容器.js
+++ leetcodeEveryday/day110/11-M-盛最多水的容器.js
@@ -12,3 +12,11 @@
   }
   return max;
 };
+// 少写几行
+var maxArea = function(height) {
+  var i = 0, j = height.length - 1, max = 0
+  while (i < j) {
+      max = Math.max(max, (j - i) * (height[i] > height[j] ? height[j--]: height[i++]))
+  }
+  return max
+};
