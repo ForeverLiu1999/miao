456075ef748fe56116210bc864c429043a62e429
Ashenone <2657518095@qq.com>
2022-02-20 21:11:50 +0800

    // 12-M-整数转罗马数字

--- leetcodeEveryday/day111/12-M-整数转罗马数字.js
+++ leetcodeEveryday/day111/12-M-整数转罗马数字.js
@@ -48,7 +48,7 @@ var intToRoman = function (num) {
   const ones = ["", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"];
   const roman = [];
   roman.push(thousands[Math.floor(num / 1000)]);
-  roman.push(thousands[Math.floor(num % 1000 / 100)]);
+  roman.push(hundreds[Math.floor(num % 1000 / 100)]);
   roman.push(tens[Math.floor(num % 100 / 10)]);
   roman.push(ones[num % 10]);
   return roman.join('');
