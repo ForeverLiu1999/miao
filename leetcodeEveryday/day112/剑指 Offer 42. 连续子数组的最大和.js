3574cb353cc82a186f310de38386a071280a8426
Ashenone <2657518095@qq.com>
2022-02-21 21:40:44 +0800

    // 剑指 Offer 42. 连续子数组的最大和

--- leetcodeEveryday/day112/剑指 Offer 42. 连续子数组的最大和.js
+++ leetcodeEveryday/day112/剑指 Offer 42. 连续子数组的最大和.js
@@ -34,17 +34,11 @@ function LSS(list) {
   return max;
 }
 
-function LSS (list) {
-  const len = list.length;
-  let max = list[0];
-  let min = 0;
-  let sum = 0;
-  for (let i = 0; i < len; i++) {
-    sum += list[i];
-    if (sum - min > max) max = sum - min;
-    if (sum < min) {
-      min = sum;
-    }
-  }
-  return max;
-}
+var maxSubArray = function(nums) {
+  let pre = 0, maxAns = nums[0];
+  nums.forEach((x) => {
+      pre = Math.max(pre + x, x);
+      maxAns = Math.max(maxAns, pre);
+  });
+  return maxAns;
+};
