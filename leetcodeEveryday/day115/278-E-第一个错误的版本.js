370c67e33b96f7cf7b0ba983d429a841d9ad0b03
Ashenone <2657518095@qq.com>
2022-02-24 22:51:53 +0800

    // 278-E-第一个错误的版本

--- leetcodeEveryday/day115/278-E-第一个错误的版本.js
+++ leetcodeEveryday/day115/278-E-第一个错误的版本.js
@@ -19,15 +19,18 @@
 // 反之第一个错误的版本必然位于该版本的左侧，缩进有边界
 // 每次判断都缩减了边界，而且每次缩减一半
 // 所以O(logn)
- var solution = function(isBadVersion) {
-  /**
-   * @param {integer} n Total versions
-   * @return {integer} The first bad version
-   */
+var solution = function(isBadVersion) {
   return function(n) {
-    let left = 1, right = n;
-    while (left < right) {
-
-    }
+      let left = 1, right = n;
+      while (left < right) { // 循环直至区间左右端点相同
+          const mid = Math.floor(left + (right - left) / 2); // 防止计算时溢出
+          if (isBadVersion(mid)) {
+              right = mid; // 答案在区间 [left, mid] 中
+          } else {
+              left = mid + 1; // 答案在区间 [mid+1, right] 中
+          }
+      }
+      // 此时有 left == right，区间缩为一个点，即为答案
+      return left;
   };
 };
