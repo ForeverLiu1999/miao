0e54fa2dea78731c40fbf01c5e955b2b6d4fabcf
Ashenone <2657518095@qq.com>
2022-02-24 21:25:26 +0800

    // 496-E-下一个更大元素 I

--- leetcodeEveryday/day115/496-E-下一个更大元素 I.js
+++ leetcodeEveryday/day115/496-E-下一个更大元素 I.js
@@ -30,16 +30,16 @@
 };
 // 预处理
 // 单调栈+哈希表
-var nextGreaterElement = function (nums1, nums2) {
+var nextGreaterElement = function(nums1, nums2) {
   const map = new Map();
   const stack = [];
   for (let i = nums2.length - 1; i >= 0; --i) {
-    const num = nums2[i];
-    while (stack.length && num >= stack[stack.lenth - 1]) {
-      stack.pop();
-    }
-    map.set(num, stack.length ? stack[stack.length - 1] : -1);
-    stack.push(num);
+      const num = nums2[i];
+      while (stack.length && num >= stack[stack.length - 1]) {
+          stack.pop();
+      }
+      map.set(num, stack.length ? stack[stack.length - 1] : -1);
+      stack.push(num);
   }
   const res = new Array(nums1.length).fill(0).map((_, i) => map.get(nums1[i]));
   return res;
