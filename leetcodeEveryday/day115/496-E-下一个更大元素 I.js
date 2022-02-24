a3666170a17ebaff93be9802910e437ac87867f5
Ashenone <2657518095@qq.com>
2022-02-24 11:44:42 +0800

    // 496-E-下一个更大元素 I

---
+++ leetcodeEveryday/day115/496-E-下一个更大元素 I.js
@@ -0,0 +1,30 @@
+// 496-E-下一个更大元素 I
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number[]}
+ */
+// 暴力解法
+ var nextGreaterElement = function(nums1, nums2) {
+  const n1 = nums1.length;
+  const n2 = nums2.length;
+  // 初始化一个与nums1等长的res
+  const res = new Array(n1).fill(0);
+  // 遍历nums1中所有元素
+  for (let i = 0; i < n1; ++i) {
+    let j = 0;
+    // 从前向后遍历nums2中的所有元素,直至找到了nums2[j]===nums1[i]
+    while (j < n2 && nums2[j] !== nums1[i]) {
+      ++j;
+    }
+    // 从j+1开始继续向后遍历,直至找到了nums2[k]>nums2[j],其中k>=j+1
+    let k = j + 1;
+    while (k < n2 && nums2[k] < nums2[j]) {
+      ++k;
+    }
+    //如果找到了nums2[k],则将res[i]置为nums2[k],否则将res[i]置为-1
+    res[i] = k < n2 ? nums2[k] : -1;
+  }
+  // 返回最终结果res
+  return res;
+};
