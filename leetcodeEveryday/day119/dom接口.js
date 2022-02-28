d662eb14d6f12285bfb8cb07f3bea96b8aedb1ad
Ashenone <2657518095@qq.com>
2022-02-28 12:05:41 +0800

    // 将node的连续文本子节点合并成一个

--- leetcodeEveryday/day119/dom接口.js
+++ leetcodeEveryday/day119/dom接口.js
@@ -1,3 +1,4 @@
+// 不带属性
 function getOuterHTML (node) {
   if (nodeType == 1) {
     return `<${node.tagName}>${Array.from(node.childNodes).map(getOuterHTML)}</${node.tagName}>`
@@ -10,4 +11,23 @@ function getOuterHTML (node) {
   }
 }
 // getOuterHTML($0)
+
+// 将node的连续文本子节点合并成一个
+function normalize (node) {
+  var sum = '';
+  var childs = Array.from(node.childNodes);
+  for (var i = 0; i < childs.length; i++) {
+    var child = childs[i];
+    if (child.nodeType == document.TEXT_NODE) {
+      sum += child.nodeValue;
+      node.removeChild(child);
+    } else {
+      node.insertBefore(document.createTextNode(sum), child)
+        sum = '';
+      }
+    }
+    if (sum.length) {
+      node.appendChild(document.createTextNode(sum));
+    }
+  }
 
