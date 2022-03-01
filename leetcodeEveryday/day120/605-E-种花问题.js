071896501089f80bee0b7a6389f956839ae493a8
Ashenone <2657518095@qq.com>
2022-03-01 20:58:28 +0800

    // 605-E-种花问题// 扫描解法

--- leetcodeEveryday/day120/605-E-种花问题.js
+++ leetcodeEveryday/day120/605-E-种花问题.js
@@ -8,3 +8,15 @@
  var canPlaceFlowers = function(flowerbed, n) {
   return Array.from(flowerbed.join('').matchAll(/(^0(?=00)|^00|00$|00(?=00)|000|^0$)/g)).length >= n
 };
+
+// 扫描解法
+var canPlaceFlowers = function (flowerbed, n) {
+  if (!n) return true;
+  for (let i = 0; i < flowerbed.length; i++) {
+    if (flowerbed[i - 1] != 1 && flowerbed[i] != 1 && flowerbed[i + 1] != 1) {
+      if (--n) return true;
+      i++;
+    }
+    return false;
+  }
+}
