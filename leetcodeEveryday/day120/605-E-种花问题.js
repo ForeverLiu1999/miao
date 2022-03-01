9ad7ee877b65bc3a5beaefde409dad7e8a469705
Ashenone <2657518095@qq.com>
2022-03-01 21:00:44 +0800

    test

--- leetcodeEveryday/day120/605-E-种花问题.js
+++ leetcodeEveryday/day120/605-E-种花问题.js
@@ -6,7 +6,7 @@
  */
 // 正则解法
  var canPlaceFlowers = function(flowerbed, n) {
-  return Array.from(flowerbed.join('').matchAll(/(^0(?=00)|^00|00$|00(?=00)|000|^0$)/g)).length >= n
+  return Array.from(flowerbed.join('').matchAll(/(^0(?=00)|^00|00$|00(?=00)|000|^0$)/g)).length >= n;
 };
 
 // 扫描解法
