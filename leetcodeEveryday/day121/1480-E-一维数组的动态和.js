3f6b1a0143910a338461b1d793fa7d5c0f50147b
Ashenone <2657518095@qq.com>
2022-03-02 22:27:04 +0800

    // 1480-E-一维数组的动态和

--- leetcodeEveryday/day121/1480-E-一维数组的动态和.js
+++ leetcodeEveryday/day121/1480-E-一维数组的动态和.js
@@ -3,9 +3,11 @@
  * @param {number[]} nums
  * @return {number[]}
  */
- var runningSum = function(nums) {
-  for (let i = 0; i < nums.length; i++) {
-    nums[i] += nums[i - 1];
-  }
-  return nums;
+// 原地解决
+var runningSum = function(nums) {
+  let n = nums.length;
+ for (let i = 1; i < n; i++) {
+   nums[i] += nums[i - 1];
+ }
+ return nums;
 };
