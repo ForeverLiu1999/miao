afe549e9250db2c95d752928b25a2ada84c1cd46
Ashenone <2657518095@qq.com>
2022-03-02 20:50:25 +0800

    // 剑指 Offer 25-E-合并两个排序的链表

---
+++ leetcodeEveryday/day121/剑指 Offer 25-E-合并两个排序的链表.js
@@ -0,0 +1,26 @@
+// 剑指 Offer 25-E-合并两个排序的链表
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
+// 递归解法
+ var mergeTwoLists = function(l1, l2) {
+   debugger;
+  if (!l1) return l2;
+  if (!l2) return l1;
+  if (l1.val < l2.val) {
+    l1.next = mergeTwoLists(l1.next, l2);
+    return l1;
+  } else {
+    l2.next = mergeTwoLists(l1, l2.next);
+    return l2;
+  }
+};
