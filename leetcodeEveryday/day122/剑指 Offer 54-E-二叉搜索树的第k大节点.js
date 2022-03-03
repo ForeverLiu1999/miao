cc0d4723cc7999b6f55e0a5c69d4ed49724d2e45
Ashenone <2657518095@qq.com>
2022-03-03 21:53:20 +0800

    // 剑指 Offer 54-E-二叉搜索树的第k大节点// 解法二 二叉搜索树的中序遍历是排好序的，所以反中序遍历然后k-1

--- leetcodeEveryday/day122/剑指 Offer 54-E-二叉搜索树的第k大节点.js
+++ leetcodeEveryday/day122/剑指 Offer 54-E-二叉搜索树的第k大节点.js
@@ -35,3 +35,18 @@
   })
   return array[k - 1];
 };
+
+// 解法二 二叉搜索树的中序遍历是排好序的，所以反中序遍历然后k-1
+var kthLargest = function (root, k) {
+  let setArray = new Set();
+  const dfs = function (node) {
+    if (node === null) {
+      return;
+    }
+    dfs(node.right);
+    setArray.push(node.val);
+    dfs(node.left);
+  }
+  dfs(root);
+  return array[k - 1];
+}
