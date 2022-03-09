b2f2370297530d81d8f61e3147d2c78e647542fe
Ashenone <2657518095@qq.com>
2022-03-09 22:33:17 +0800

    // 面试题 01.02-E-判定是否互为字符重排

--- leetcodeEveryday/day127/面试题 01.02-E-判定是否互为字符重排.js
+++ leetcodeEveryday/day127/面试题 01.02-E-判定是否互为字符重排.js
@@ -7,3 +7,22 @@
  var CheckPermutation = function(s1, s2) {
   return s1.split('').sort().toString() === s2.split('').sort().toString();
 };
+
+// 抵消法 找到一个删除一个
+var CheckPermutation = function (s1, s2) {
+  // 如果长度不对等直接返回false
+  if (s1.length != s2.length) {
+    return false;
+  }
+  // s2转换为数组
+  s2 = s2.split('');
+  // 遍历s1
+  for (let s of s1) {
+    if (s2.indexOf(s) == -1) {
+      return false;
+    } else {
+      s2.splice(s2.indexOf(s), 1);
+    }
+  }
+  return true;
+}
