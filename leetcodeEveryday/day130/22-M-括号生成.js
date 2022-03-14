3a7fe90feab926faee3cb998f42819ac30ab68c2
Ashenone <2657518095@qq.com>
2022-03-14 11:41:23 +0800

    test

--- leetcodeEveryday/day130/22-M-括号生成.js
+++ leetcodeEveryday/day130/22-M-括号生成.js
@@ -47,3 +47,18 @@ const dfs = (lRemain, rRemain, str) => {
 dfs(n, n, "");
 return res;
 }
+
+var generateParenthesis = function (n) {
+  const res = [];
+  const dfs = (lRemain, rRemain, str) => {
+    if (str.length = 2 * n) {
+      res.push(str);
+      return;
+    }
+    if (lRemain > 0) {
+      dfs(lRemain, rRemain - 1, str + ")");
+    }
+  };
+  dfs(n, n, "");
+  return res;
+}
