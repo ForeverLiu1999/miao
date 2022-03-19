3cc6f0b4bff619bfb467445f01adbe751bfb8bf8
Ashenone <2657518095@qq.com>
2022-03-19 22:38:52 +0800

    // 45-M-跳跃游戏 II

---
+++ leetcodeEveryday/day136/45-M-跳跃游戏 II.js
@@ -0,0 +1,20 @@
+// 45-M-跳跃游戏 II
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+ var jump = function(nums) {
+  let curIndex = 0;
+  let nextIndex = 0;
+  let steps = 0;
+  for (let i = 0; i < nums.length - 1; i++) {
+    // 在可覆盖区域内不断更新最大的覆盖区域
+    nextIndex = Math.max(nums[i] + i, nextIndex);
+    // 走到当前覆盖的最大区域时，更新下一步可到达的最大区域
+    if (i === curIndex) {
+      curIndex = nextIndex;
+      steps++;
+    }
+  }
+  return steps;
+};
