79e6718ad74c148bbc6f6baa17d07d6f782c6dcb
Ashenone <2657518095@qq.com>
2022-03-20 14:47:26 +0800

    test

--- leetcodeEveryday/day136/45-M-跳跃游戏 II.js
+++ leetcodeEveryday/day136/45-M-跳跃游戏 II.js
@@ -32,3 +32,17 @@ var jump = function (nums) {
   }
   return steps;
 }
+
+var jump = function (nums) {
+  let curIndex = 0;
+  let nextIndex = 0;
+  let steps = 0;
+  for (let i = 0; i < nums.length - 1; i++) {
+    nextIndex = Math.max(nums[i] + 1, nextIndex);
+    if (i === curIndex) {
+      curIndex = nextIndex;
+      steps++;
+    }
+  }
+  return steps;
+}
