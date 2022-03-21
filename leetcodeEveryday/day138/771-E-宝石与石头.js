7dbc701bdba6d5697fdf0e16114d77b926f70f1f
Ashenone <2657518095@qq.com>
2022-03-21 12:12:54 +0800

    // 771-E-宝石与石头// 对暴力遍历经典优化方法就是哈希集合

--- leetcodeEveryday/day138/771-E-宝石与石头.js
+++ leetcodeEveryday/day138/771-E-宝石与石头.js
@@ -4,6 +4,7 @@
  * @param {string} stones
  * @return {number}
  */
+// 简单粗暴暴力遍历，遍历stones中每个字母，看jewels中有没有一样的
  var numJewelsInStones = function(jewels, stones) {
   jewels = jewels.split('');
   return stones.split('').reduce((prev, val) => {
@@ -15,3 +16,13 @@
     return prev;
   }, 0);
 };
+
+// 对暴力遍历经典优化方法就是哈希集合
+// 先存储后查找，可以节约时间复杂度
+// 但也会导致空间复杂度提高
+var numJewelsInStones = function (jewels, stones) {
+  const jewelsSet = new Set(jewels.split(''));
+  return stones.split('').reduce((prev, val) => {
+    return prev + jewelsSet.has(val);
+  }, 0);
+}
