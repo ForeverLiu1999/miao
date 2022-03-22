379e7c27dc07d23fb1a589e92d4abc262d592b11
Ashenone <2657518095@qq.com>
2022-03-22 15:18:40 +0800

    // 389-E-找不同// 字符串的t所有字母ascll码之和减去字符串s所有字母ascll码之和

--- leetcodeEveryday/day139/389-E-找不同.js
+++ leetcodeEveryday/day139/389-E-找不同.js
@@ -4,24 +4,35 @@
  * @param {string} t
  * @return {character}
  */
- var findTheDifference = function(s, t) {
+var findTheDifference = function (s, t) {
   //  计数值
   const cnt = new Array(26).fill(0);
   // 首先遍历s
   for (const ch of s) {
     // 对其中每个字母都将计数值加一
-      cnt[ch.charCodeAt() - 'a'.charCodeAt()]++;
+    cnt[ch.charCodeAt() - 'a'.charCodeAt()]++;
   }
   // 然后遍历t
   for (const ch of t) {
     // 对其中每个字母都将计数值减一
-      cnt[ch.charCodeAt() - 'a'.charCodeAt()]--;
-      // 而如果有字母计数为负说明这个字母在t中比在s中多出现一次，所以他就是被添加的字母
-      if (cnt[ch.charCodeAt() - 'a'.charCodeAt()] < 0) {
-          return ch;
-      }
+    cnt[ch.charCodeAt() - 'a'.charCodeAt()]--;
+    // 而如果有字母计数为负说明这个字母在t中比在s中多出现一次，所以他就是被添加的字母
+    if (cnt[ch.charCodeAt() - 'a'.charCodeAt()] < 0) {
+      return ch;
+    }
   }
   return ' ';
 };
 
-//
+// 字符串的t所有字母ascll码之和减去字符串s所有字母ascll码之和
+var findTheDifference = function (s, t) {
+  let sSum = 0,
+    tSum = 0;
+  for (let i of s) {
+    sSum += s[i].charCodeAt();
+  }
+  for (let j of t) {
+    tSum += t[i].charCodeAt();
+  }
+  return String.formCharCode(tSum - sSum);
+}
