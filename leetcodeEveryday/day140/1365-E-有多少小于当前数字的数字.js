394899e8691be381c55d7df4404e7306a3e24ef2
Ashenone <2657518095@qq.com>
2022-03-23 22:21:33 +0800

    test

--- leetcodeEveryday/day140/1365-E-有多少小于当前数字的数字.js
+++ leetcodeEveryday/day140/1365-E-有多少小于当前数字的数字.js
@@ -23,12 +23,15 @@
 // 先排序，记录每一个数在原数组的位置，对于排序后数组中的每一个数，
 // 选择其左侧第一个小于他的数，这样就得到了小于当前数字的数字
 var smallerNumbersThanCurrent = function (nums) {
+  let n = nums.length;
   // 创建一个二维数组同时保存value和idex
   const data = new Array(n).fill(0).map(v => new Array(2).fill(0));
+  // 遍历nums所有元素和对应原索引，放入data中
   for (let i = 0; i < n; i++) {
     data[i][0] = nums[i];
     data[i][1] = i;
   }
+  // 排序
   data.sort((a, b) => a[0] - b[0]);
   const ret = new Array(n);
   let prev = -1;
@@ -40,3 +43,20 @@ var smallerNumbersThanCurrent = function (nums) {
   }
   return ret;
 }
+
+// 数组范围0-100,建立一个频次数组cnt,cnt[i]代表i出现的次数，那么对于数字i来说，小于他的数目为cnt[0...i-1]
+var smallerNumbersThanCurrent = function(nums) {
+  const cnt = new Array(101).fill(0);
+  const n = nums.length;
+  for (let i = 0; i < n; ++i) {
+      cnt[nums[i]] += 1;
+  }
+  for (let i = 1; i <= 100; ++i) {
+      cnt[i] += cnt[i - 1];
+  }
+  const ret = [];
+  for (let i = 0; i < n; ++i) {
+      ret.push(nums[i] ? cnt[nums[i] - 1] : 0);
+  }
+  return ret;
+};
