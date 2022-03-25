d77dd9f4e657b26faf2a26629aed58e019665e6e
Ashenone <2657518095@qq.com>
2022-03-25 21:14:13 +0800

    // 1047-E-删除字符串中的所有相邻重复项

---
+++ leetcodeEveryday/day142/1047-E-删除字符串中的所有相邻重复项.js
@@ -0,0 +1,18 @@
+// 1047-E-删除字符串中的所有相邻重复项
+/**
+ * @param {string} s
+ * @return {string}
+ */
+// 为防止消去导致相邻又消去，所以用栈来解决
+//  var removeDuplicates = function(s) {
+//    let result1 = [];
+//    s = s.sort((a, b) => a - b);
+//    for (let i = 0; i < s.length; i++) {
+//     if (s[i] == s[i + 1]) result1.push(s[i]);
+//    }
+//   let set = new Set();
+//   for (let j of result1) {
+//     set.add(result1[j]);
+//   }
+//   return set.join("");
+// };
