67356f031044eb01304e0582ce3d68100b5e9d0e
Ashenone <2657518095@qq.com>
2022-03-25 22:33:22 +0800

    // 剑指 Offer 55-E-II. 平衡二叉树

---
+++ leetcodeEveryday/day142/剑指 Offer 55-E-II. 平衡二叉树.js
@@ -0,0 +1,15 @@
+// 剑指 Offer 55-E-II. 平衡二叉树
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+ var isBalanced = function(root) {
+
+};
