50acc74a9127f78cc38b60fdd838c2a5dce57ba0
Ashenone <2657518095@qq.com>
2022-03-25 21:21:23 +0800

    // 1047-E-删除字符串中的所有相邻重复项

--- leetcodeEveryday/day142/1047-E-删除字符串中的所有相邻重复项.js
+++ leetcodeEveryday/day142/1047-E-删除字符串中的所有相邻重复项.js
@@ -16,3 +16,15 @@
 //   }
 //   return set.join("");
 // };
+
+var removeDuplicates = function (s) {
+  let stack = [];
+  for (let i of s) {
+    if (stack.length && stack[stack.length - 1] === i) {
+    stack.pop();
+    } else {
+      stack.push(i);
+    }
+  }
+  return stack.join('');
+}
