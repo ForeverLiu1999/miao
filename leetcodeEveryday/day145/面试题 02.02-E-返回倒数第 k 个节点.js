f5af87f4a39a2280f5890e41fb2d6c42903ac7b3
Ashenone <2657518095@qq.com>
2022-03-28 22:24:23 +0800

    // 面试题 02.02-E-返回倒数第 k 个节点

---
+++ leetcodeEveryday/day145/面试题 02.02-E-返回倒数第 k 个节点.js
@@ -0,0 +1,43 @@
+// 面试题 02.02-E-返回倒数第 k 个节点
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} k
+ * @return {number}
+ */
+ 优雅双指针,快慢指针相隔k,则快指针结束时,慢指针指向倒数第k个节点
+ var kthToLast = function(head, k) {
+  let slow = head;
+  let fast = head;
+  // fast先移动 k + 1 次
+  for (let i = 0; i < k; i++) {
+    fast = fast.next;
+  }
+  // 同时移动快慢指针
+  while (fast) {
+    slow = slow.next;
+    fast = fast.next;
+  }
+  return slow.val;
+};
+
+//  优雅双指针,快慢指针相隔k,则快指针结束时,慢指针指向倒数第k个节点
+const kthToLast = (head, k) => {
+  let fast = head, slow = head;
+  // 快指针先走
+  while (k--) {
+    fast = fast.next;
+  }
+//   // 同时移动快慢指针
+  while (fast) {
+    fast = fast.next;
+    slow = slow.next;
+  }
+  return slow.val;
+}
