811781dabd9d0b45bbb2bd24a64ffc6bf8dae474
Ashenone <2657518095@qq.com>
2022-04-02 22:21:16 +0800

    test

--- leetcodeEveryday/day148/1046-E-最后一块石头的重量.js
+++ leetcodeEveryday/day148/1046-E-最后一块石头的重量.js
@@ -3,10 +3,24 @@
  * @param {number[]} stones
  * @return {number}
  */
+// 最大堆
  var lastStoneWeight = function(stones) {
-   stones = stones.sort((a, b) => a - b);
-  for (let i = 0; i < stones.length; i++) {
-    let firstHeavyStones = stones[stones.length - 1];
-    let secondHeavyStones = stones[stones.length - 2];
+  //  创建堆
+  const pq = new MaxPriorityQueue();
+  // 遍历stones压入pq中
+  for (const stone of stones) {
+    pq.enqueue('x', stone);
   }
+  // 直到剩下石头少于两块
+  while (pq.size() > 1) {
+    // a、b分别是第一重、第二重的石头
+    const a = pq.dequeue()['priority'];
+    const b = pq.dequeue()['priority'];
+    // 如果a > b就把 a - b的值放入堆中
+    if (a > b) {
+      pq.enqueue('x', a - b);
+    }
+  }
+  // 不为空就是只剩一块石头,返回他
+  return pq.isEmpty() ? 0 : pq.dequeue()['priority'];
 };
