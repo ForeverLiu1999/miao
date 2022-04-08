642932bb30905d13bfad8580913dd8bc2775c9a8
Ashenone <2657518095@qq.com>
2022-04-08 14:18:59 +0800

    // 48-M-旋转图像

--- leetcodeEveryday/day155/48-M-旋转图像.js
+++ leetcodeEveryday/day155/48-M-旋转图像.js
@@ -4,7 +4,7 @@
  * @return {void} Do not return anything, modify matrix in-place instead.
  */
 // 辅助数组
-// 第i行第j个元素旋转后出现在倒数第i行第j个元素
+// 第i行第j个元素旋转后出现在倒数第i列第j个位置
 // matrix[row][col] => matrixNew[col][n - row - 1]
  var rotate = matrix => {
   const n = matrix.length;
@@ -15,6 +15,7 @@
       matrixNew[j][n - i - 1] = matrix[i][j];
     }
   }
+  // 在原数组上进行修改
   for (let i = 0; i < n; i++) {
      for (let j = 0; j < n; j++) {
        matrix[i][j] = matrixNew[i][j];
