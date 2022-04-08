5179a56e9779c6a63075c57b10c51358c8092092
Ashenone <2657518095@qq.com>
2022-04-08 14:04:25 +0800

    48-M-旋转图像

---
+++ leetcodeEveryday/day155/48-M-旋转图像.js
@@ -0,0 +1,23 @@
+// 48-M-旋转图像
+/**
+ * @param {number[][]} matrix
+ * @return {void} Do not return anything, modify matrix in-place instead.
+ */
+// 辅助数组
+// 第i行第j个元素旋转后出现在倒数第i行第j个元素
+// matrix[row][col] => matrixNew[col][n - row - 1]
+ var rotate = matrix => {
+  const n = matrix.length;
+  // 创建n行n列二维数组
+  const matrixNew = new Array(n).fill(0).map(() => new Array(n).fill(0));
+  for (let i = 0; i < n; i++) {
+    for (let j = 0; j < n; j++) {
+      matrixNew[j][n - i - 1] = matrix[i][j];
+    }
+  }
+  for (let i = 0; i < n; i++) {
+     for (let j = 0; j < n; j++) {
+       matrix[i][j] = matrixNew[i][j];
+     }
+  }
+};
