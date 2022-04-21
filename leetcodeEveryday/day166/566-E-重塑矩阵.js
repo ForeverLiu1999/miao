fe28c5453173320270614f29e418b035c4143712
Ashenone <2657518095@qq.com>
2022-04-21 11:00:08 +0800

    TEST

--- leetcodeEveryday/day166/566-E-重塑矩阵.js
+++ leetcodeEveryday/day166/566-E-重塑矩阵.js
@@ -14,7 +14,7 @@
   }
   const ans = new Array(r).fill(0).map(() => new Array(c).fill(0));
   for (let x = 0; x < m * n; ++x) {
-      // 对于任意x[0, mn),第x个元素在nums中下标为(x / n, x % n)
+      // 对于任意x[0, mn),第x个元素在nums中下标为(x / n, x % n)7654321`
       // 而在新的重塑矩阵中对应下标为(x / c, x % c)
       ans[Math.floor(x / c)][x % c] = nums[Math.floor(x / n)][x % n];
   }
