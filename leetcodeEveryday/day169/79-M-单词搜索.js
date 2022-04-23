e505f535d60d53665c9a404c1d47cf371fc7850f
Ashenone <2657518095@qq.com>
2022-04-23 22:12:36 +0800

    // 79-M-单词搜索

--- leetcodeEveryday/day169/79-M-单词搜索.js
+++ leetcodeEveryday/day169/79-M-单词搜索.js
@@ -4,6 +4,32 @@
  * @param {string} word
  * @return {boolean}
  */
- var exist = function(board, word) {
+// 递归
+ const exist = function(board, word) {
+  const m = board.length;
+  const n = board[0].length;
+  const used = new Array(m); // 二维矩阵used,存放bool值
+  for (let i = 0; i < m; i++) {
+    used[i] = new Array(n);
+  }
+ // canFind判断房钱点是否是目标路径上的点
+ const canFind = (row, col, i) => { // row col 当前点的左标,i当前考察的word字符索引
+  if (i == word.length) { // 递归的出口,i越界了就返回true
+    return true;
+  }
+  if (row < 0 || row >= m || col < 0 || col >= n) { // 当前点越界,返回false
+    return false;
+  }
+  if (used[row][col] || board[row][col] != word[i]) { // 当前点已经访问过,或非目标点
+    return false;
+  }
+  // 排除所有false的情况,当前点暂时没毛病,可以继续递归考察
+  used[row][col] = true; // 记录一下当前点被访问
+  // canFindRest: 基于当前选择的点[row, col],能否找到剩余字符的路径
+  const canFindRest = canFind(row + 1, col, i + 1) || canFind(row - 1, col, i + 1) || canFind(row + 1, i + 1) || canFind(row, col = 1, i + 1);
+
+
+
+ }
 
 };
