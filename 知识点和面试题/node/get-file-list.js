744779f41de635db631c568db99150bdc2ac760e
Ashenone <2657518095@qq.com>
2022-05-03 22:17:41 +0800

    get-file-list.js 异步版本

--- 知识点和面试题/node/get-file-list.js
+++ 知识点和面试题/node/get-file-list.js
@@ -22,25 +22,33 @@ function getFileListSync (dir) {
 
 // 异步版本
 async function getFileList (dir) {
-  var reusult = [];
+  var result = [];
   var entries = await fsp.readdir (dir, { withFileTypes: true });
   for (var entry of entries) {
     var entryPath = path.join(dir, entry.name);
     if (entry.isFile()) {
-      result.push(entrypath);
+      result.push(entryPath);
     } else if (entry.isDirectory()) {
-      var resultOfChild = await getFileList(antryPath);
+      var resultOfChild = await getFileList(entryPath);
       result.push(...resultOfChild);
     }
   }
   return result;
 }
 
+// 不能直接console,await会被识别为一个变量
+// console.log(await, getFileList('./test-a'));
+
+// 所以可以放进一个异步函数内然后调用
 async function main () {
   console.log(await getFileList('./test-a'));
 }
 main();
 
-// getFileList ('./test-a').then(result => {
+// 如果直接输出会得到一个promise
+// console.log (getFileList('./test-a'));
+// 所以也可以这样
+// getFileList('./test-a').then(result => {
 //   console.log(result);
 // })
+
