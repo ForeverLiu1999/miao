39cf1b099845366401c7940acada0e4e660f11ff
Ashenone <2657518095@qq.com>
2022-07-27 15:12:03 +0800

    基本路由配置完成

--- aria2-ui-react/src/App.tsx
+++ aria2-ui-react/src/App.tsx
@@ -4,6 +4,8 @@ import { HashRouter, NavLink, Routes, Route } from 'react-router-dom';
 
 import Downloading from './Downloading';
 import Waiting from './Downloading';
+import NewTask from './NewTask';
+import Header from './Header';
 
 import Aria2Client from './aria2-client'; // 手动创建连接aria2
 
@@ -18,27 +20,26 @@ function App() {
           <div><NavLink to="/downloading">下载中</NavLink></div>
           <div><NavLink to="/waiting">等待中</NavLink></div>
           <div><NavLink to="/completed">已完成</NavLink></div>
+          <div><NavLink to="/new">新建任务</NavLink></div>
         </div>
         <div className="App--right">
           <div className="App-header">
-            <button>新建下载</button>
-          </div>
-          <div>
-            <Routes>
-              <Route path='/downloading' element={<Downloading />}>
-              </Route>
-              <Route path='/waiting' element={<Waiting />}>
-              </Route>
-              <Route path='/completed' >
-              </Route>
-            </Routes>
+            <Header />
           </div>
         </div>
-
+        <div>
+          <Routes>
+            <Route path='/downloading' element={<Downloading />}>
+            </Route>
+            <Route path='/waiting' element={<Waiting />}>
+            </Route>
+            <Route path='/new' element={<NewTask />}>
+            </Route>
+          </Routes>
+        </div>
       </div>
-
-    </HashRouter>
-  );
+    </HashRouter >
+  )
 }
 
 export default App;
---
+++ aria2-ui-react/src/NewTask.tsx
@@ -0,0 +1,15 @@
+export default function NewTask() {
+  return (
+    <div>
+      <div>
+        选项
+      </div>
+      <div>
+        <div>
+          下载链接,一行一个:
+        </div>
+        <textarea name="" id="" cols={30} rows={10}></textarea>
+      </div>
+    </div>
+  )
+}
