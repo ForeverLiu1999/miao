b14f247ceaec0ae44f92d51fa0336e6d893198a4
Ashenone <2657518095@qq.com>
2022-08-11 22:12:55 +0800

    服务器配置

--- aria2-ui-react/src/App.tsx
+++ aria2-ui-react/src/App.tsx
@@ -17,26 +17,55 @@ import Servers from './Servers';
 globalThis.Aria2Client = Aria2Client;
 
 function App() {
-  var aria2 = useMemo(() => new Aria2Client('127.0.0.1', '11000', '111222333'), [])
+  var curentServerIdx = useMemo(() => localStorage.currentServerIdx ?? 0, []); // 如果不存在就用0,从localStorage里边读一次以后就不用变了.
+
+  var aria2Servers = useMemo(() => { // 从localStorage取出的已经保存的服务器.
+    return JSON.parse(localStorage.ARIA2_SERVERS ?? "[]"); // 传一个字符串,解析为空数组.
+  }, [])
+
+  var [aria2, setAria2] = useState(
+    useMemo(() => {
+      var server = aria2Servers[curentServerIdx]
+      var aria2 = new Aria2Client(server.ip, server.port, server.secret)
+      return aria2
+    }, [])
+  )
+
   var [selectedTasks, setSelectedTasks] = useState([])
 
   var listComp = useRef()
 
+
+
+  function changeServer(e: React.ChangeEvent<HTMLSelectElement>) { // 把旧的服务器关掉,往下层传递一个新的aria2对象,
+    var idx = e.target.value
+    var server = aria2Servers[idx]
+    setAria2(new Aria2Client(server.ip, server.port, server.secret))
+  }
+
   return (
     // Context.provider的用法就是在组件上一层包起来.
-    <SelectedTasksContext.Provider value={{selectedTasks, setSelectedTasks}}>
+    <SelectedTasksContext.Provider value={{ selectedTasks, setSelectedTasks }}>
       <HashRouter>
         <div className="App">
           <div className="App-left">
+            <select onChange={(e) => changeServer(e)} value={0}>
+              {
+                aria2Servers.map((server: any, idx: number) => {
+                  return <option key={server.ip} value={idx}>{server.ip}</option> // server对象的ip属性.
+                })
+              }
+            </select>
             <div><NavLink style={({ isActive }) => ({ color: isActive ? 'red' : '' })} to="/downloading">下载中</NavLink></div>
             <div><NavLink style={({ isActive }) => ({ color: isActive ? 'red' : '' })} to="/waiting">等待中</NavLink></div>
             <div><NavLink style={({ isActive }) => ({ color: isActive ? 'red' : '' })} to="/stopped">已完成</NavLink></div>
             <div><NavLink style={({ isActive }) => ({ color: isActive ? 'red' : '' })} to="/settings">设置</NavLink></div>
+            <div><NavLink style={({ isActive }) => ({ color: isActive ? 'red' : '' })} to="/servers">服务器</NavLink></div>
             {/* <div><NavLink to="/new">新建任务</NavLink></div> */}
           </div>
           <div className="App--right">
             <div className="App-header">
-              <Header listComp={listComp}/>
+              <Header listComp={listComp} />
             </div>
           </div>
           <div>
@@ -52,7 +81,7 @@ function App() {
               </Route>
               <Route path='/settings' element={<Settings client={aria2} />}>
               </Route>
-              <Route path='/服务器管理' element={<Servers />}>
+              <Route path='/servers' element={<Servers />}>
               </Route>
               <Route path='/task/detail/:gid' element={<TaskDetail client={aria2} />}>
               </Route>
--- aria2-ui-react/src/Servers.tsx
+++ aria2-ui-react/src/Servers.tsx
@@ -1,17 +1,56 @@
-export default function Servers () {
+import { getValue } from "@testing-library/user-event/dist/utils"
+import { useMemo, useState } from "react"
+import { useInput } from "./hooks"
 
+export default function Servers() {
+  var ip: any = useInput('')
+  var port: any = useInput('')
+  var secret: any = useInput('')
+
+  var aria2Servers = useMemo(() => {
+    return JSON.parse(localStorage.ARIA2_SERVERS ?? "[]"); // 传一个字符串,解析为空数组.
+  }, [])
+
+  var [servers, setServers] = useState(aria2Servers)
+
+  function addServer() {
+    var newServers = [...servers, { // 这里的ip port secret是对象,所以要取出对象的value属性.
+      ip: ip.value,
+      port: port.value,
+      secret: secret.value
+    }]
+
+    setServers(newServers)
+
+    localStorage.ARIA2_SERVERS = JSON.stringify(newServers) // 存入localStorage中.
+
+    ip.clear()
+    port.clear()
+    secret.clear()
+  }
 
   return (
     <div>
       <h2>服务器管理</h2>
       <ul>
-        <li>列出服务器</li>
+        {
+          servers.map((server: any) => {
+            return (
+              <li>
+                <div>ip: {server.ip}</div>
+                <div>port: {server.port}</div>
+                <div>secret: {server.secret}</div>
+              </li>
+            )
+          })
+        }
       </ul>
       <form>
-        <div>ip: <input type="text" /></div>
-        <div>port: <input type="text" /></div>
-        <div>secret: <input type="text" /></div>
-        <button>添加</button>
+        <div>ip: <input type="text" {...ip} /></div>
+        <div>port: <input type="text" {...port} /></div>
+        <div>secret: <input type="text" {...secret} /></div>
+        {/* form内的button如果不设置type,默认是submit,点击button就会提交form,所以要修改类型,当然也可以去掉form. */}
+        <button type="button" onClick={addServer}>添加</button>
       </form>
     </div>
 
