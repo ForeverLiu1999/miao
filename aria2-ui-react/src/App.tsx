964dab62216ca7393bef467fc387ec629ad05723
Ashenone <2657518095@qq.com>
2022-08-13 22:22:46 +0800

    Eloquent JavaScript

--- aria2-ui-react/src/App.tsx
+++ aria2-ui-react/src/App.tsx
@@ -12,6 +12,8 @@ import TaskDetail from './TaskDetail';
 import { SelectedTasksContext } from './hooks';
 import Settings from './Settings';
 import Servers from './Servers';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css'
 
 // @ts-ignore 让typescript不再检查这一行
 globalThis.Aria2Client = Aria2Client;
@@ -21,6 +23,7 @@ function App() {
 
   var [connectState, setConnectState] = useState("连接中...")
   var [selectedIdx, setSelectedIdx] = useState("0")
+  var [globalStat, setGlobalStat] = useState<any>({})
 
   var aria2Servers = useMemo(() => { // 从localStorage取出的已经保存的服务器.
     return JSON.parse(localStorage.ARIA2_SERVERS ?? "[]"); // 传一个字符串,解析为空数组.
@@ -30,7 +33,7 @@ function App() {
     useMemo(() => {
       var server = aria2Servers[curentServerIdx]
       var aria2 = new Aria2Client(server.ip, server.port, server.secret)
-     return aria2
+      return aria2
     }, [])
   )
 
@@ -40,9 +43,34 @@ function App() {
     }, () => {
       setConnectState('连接失败.')
     })
-    aria2.on('DownloadComplete', async (taskInfo) => {
 
+    // 监听setInterval应该在ready之后
+    var id: NodeJS.Timer // setInterval返回值类型是NodeJS.Timer
+    aria2.ready().then(() => {
+      id = setInterval(() => {
+        // @ts-ignore
+        aria2.getGlobalStat().then(stat => {
+          setGlobalStat(stat)
+        })
+      }, 1000)
     })
+
+    //@ts-ignore
+    globalThis.toast = toast
+    async function onComplete(taskInfo: any) {
+      // @ts-ignore
+      var task = await aria2.tellStatus(taskInfo.gid)
+      toast('有任务下载完成了.', task.files[0].path) // 显示信息.
+    }
+    aria2.ready().then(() => {
+      aria2.on('DownloadComplete', onComplete)
+    })
+
+    return () => {
+      clearInterval(id)
+      aria2.off('DownloadComplete', onComplete)
+
+    }
   }, [aria2])
 
   var [selectedTasks, setSelectedTasks] = useState([])
@@ -61,10 +89,11 @@ function App() {
   return (
     // Context.provider的用法就是在组件上一层包起来.
     <SelectedTasksContext.Provider value={{ selectedTasks, setSelectedTasks }}>
+      <ToastContainer />
       <HashRouter>
         <div className="App">
           <div className="App-left">
-          {/* 就地写select元素的change事件,就地写的好处是不用传给别人,不需要考虑类型问题. */}
+            {/* 就地写select元素的change事件,就地写的好处是不用传给别人,不需要考虑类型问题. */}
             <select onChange={changeServer} value={selectedIdx}>
               {
                 aria2Servers.map((server: any, idx: number) => {
@@ -73,12 +102,15 @@ function App() {
               }
             </select>
             <div>{connectState}</div>
-            <div><NavLink style={({ isActive }) => ({ color: isActive ? 'red' : '' })} to="/downloading">下载中</NavLink></div>
-            <div><NavLink style={({ isActive }) => ({ color: isActive ? 'red' : '' })} to="/waiting">等待中</NavLink></div>
-            <div><NavLink style={({ isActive }) => ({ color: isActive ? 'red' : '' })} to="/stopped">已完成</NavLink></div>
+            <div><NavLink style={({ isActive }) => ({ color: isActive ? 'red' : '' })} to="/downloading">下载中({globalStat.numActive})</NavLink></div>
+            <div><NavLink style={({ isActive }) => ({ color: isActive ? 'red' : '' })} to="/waiting">等待中({globalStat.numWaiting})</NavLink></div>
+            <div><NavLink style={({ isActive }) => ({ color: isActive ? 'red' : '' })} to="/stopped">已完成({globalStat.numStoppedTotal})</NavLink></div>
             <div><NavLink style={({ isActive }) => ({ color: isActive ? 'red' : '' })} to="/settings">设置</NavLink></div>
             <div><NavLink style={({ isActive }) => ({ color: isActive ? 'red' : '' })} to="/servers">服务器</NavLink></div>
             {/* <div><NavLink to="/new">新建任务</NavLink></div> */}
+            <hr />
+            <div>上传: {globalStat.uploadSpeed} B/s</div>
+            <div>下载: {globalStat.downloadSpeed} B/s</div>
           </div>
           <div className="App--right">
             <div className="App-header">
